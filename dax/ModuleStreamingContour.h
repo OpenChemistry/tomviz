980422bbf681fc0f508e2c06259d512a4767b32e
Robert Maynard <robert.maynard@kitware.com>
2014-07-11 13:01:16 -0400

    Updates all the AccellModules to work with DataSources.

--- dax/ModuleStreamingContour.h
+++ dax/ModuleStreamingContour.h
@@ -35,7 +35,7 @@ public:
 
   virtual QString label() const { return  "Accelerated Contour"; }
   virtual QIcon icon() const;
-  virtual bool initialize(vtkSMSourceProxy* dataSource, vtkSMViewProxy* view);
+  virtual bool initialize(DataSource* dataSource, vtkSMViewProxy* view);
   virtual bool finalize();
   virtual bool setVisibility(bool val);
   virtual bool visibility() const;
