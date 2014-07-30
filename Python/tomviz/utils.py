17faea99343e37036b7ee35e5d3273f98a52dba9
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2014-07-30 18:25:14 -0400

    Fix numpy related errors on Mavericks.
    The problem was due to the fact that operations (like sqrt) can return a
    float16 arrays which cannot be passed back to VTK directly. Added a
    temporary conversion to float64. We should potentially handle this in
    VTK.

--- Python/tomviz/utils.py
+++ Python/tomviz/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import vtk.numpy_interface.dataset_adapter as dsa
+import vtk.util.numpy_support as np_s
 
 def get_scalars(dataobject):
     do = dsa.WrapDataObject(dataobject)
@@ -15,5 +16,12 @@ def set_scalars(dataobject, newscalars):
     oldscalars = do.PointData.GetScalars()
     name = oldscalars.GetName()
     del oldscalars
+
+    # handle the case if the newscalars array has a type that
+    # cannot be passed on to VTK. In which case, we convert to
+    # convert to float64
+    vtk_typecode = np_s.get_vtk_array_type(newscalars.dtype)
+    if vtk_typecode is None:
+        newscalars = newscalars.astype(np.float64)
     do.PointData.append(newscalars, name)
     do.PointData.SetActiveScalars(name)
