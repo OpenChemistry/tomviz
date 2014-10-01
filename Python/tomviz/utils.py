663c622eee578ef9ef739cd2687a4716b3c92540
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-10-01 19:09:10 -0400

    Extended the Python utilities 3D numpy arrays
    This adds two new functions to get and set 3D numpy arrays, note
    that we want Fortran ordered 3D arrays to avoid expensive deep
    copies, but could add code to convert (warn right now if C order).

--- Python/tomviz/utils.py
+++ Python/tomviz/utils.py
@@ -10,7 +10,6 @@ def get_scalars(dataobject):
     vtkarray.Association = dsa.ArrayAssociation.POINT
     return vtkarray
 
-
 def set_scalars(dataobject, newscalars):
     do = dsa.WrapDataObject(dataobject)
     oldscalars = do.PointData.GetScalars()
@@ -24,4 +23,27 @@ def set_scalars(dataobject, newscalars):
     if vtk_typecode is None:
         newscalars = newscalars.astype(np.float64)
     do.PointData.append(newscalars, name)
+    do.PointData.SetActiveScalars(name)
+
+def get_array(dataobject):
+    scalars_array = get_scalars(dataobject)
+    scalars_array3d = np.reshape(scalars_array, (dataobject.GetDimensions()),
+                                 order='F')
+    return scalars_array3d
+
+def set_array(dataobject, newarray):
+    if not np.isfortran(newarray):
+        print 'Warning, new array did not have Fortran order.'
+    dataobject.SetExtent(0, newarray.shape[0] - 1,
+                         0, newarray.shape[1] - 1,
+                         0, newarray.shape[2] - 1)
+    arr = newarray.reshape(-1, order='F')
+    vtkarray = np_s.numpy_to_vtk(arr)
+    vtkarray.Association = dsa.ArrayAssociation.POINT
+
+    do = dsa.WrapDataObject(dataobject)
+    oldscalars = do.PointData.GetScalars()
+    name = oldscalars.GetName()
+    del oldscalars
+    do.PointData.append(arr, name)
     do.PointData.SetActiveScalars(name)
