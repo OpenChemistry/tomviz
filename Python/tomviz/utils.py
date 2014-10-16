b5d9f170ade3dcb4d6d10a3dca6e236fa1eeeb12
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-10-16 16:59:19 -0400

    Perform the deep copy/transform to Fortan order
    If the array is in Fortran order just use reshape, otherwise convert
    to Fortran order after warning.

--- Python/tomviz/utils.py
+++ Python/tomviz/utils.py
@@ -32,15 +32,23 @@ def get_array(dataobject):
     return scalars_array3d
 
 def set_array(dataobject, newarray):
-    if not np.isfortran(newarray):
-        print 'Warning, new array did not have Fortran order.'
+    # Ensure we have Fortran ordered flat array to assign to image data. This
+    # is ideally done without additional copies, but if C order we must copy.
+    if np.isfortran(newarray):
+        arr = newarray.reshape(-1, order='F')
+    else:
+        print 'Warning, new array did not have Fortran order, deep copy needed.'
+        tmp = np.asfortranarray(newarray)
+        arr = tmp.reshape(-1, order='F')
+
+    # Set the extents (they may have changed).
     dataobject.SetExtent(0, newarray.shape[0] - 1,
                          0, newarray.shape[1] - 1,
                          0, newarray.shape[2] - 1)
-    arr = newarray.reshape(-1, order='F')
+
+    # Now replace the scalars array with the new array.
     vtkarray = np_s.numpy_to_vtk(arr)
     vtkarray.Association = dsa.ArrayAssociation.POINT
-
     do = dsa.WrapDataObject(dataobject)
     oldscalars = do.PointData.GetScalars()
     name = oldscalars.GetName()
