d832b93e4d1afe084a458b3d6a2ce6928fcef20f
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-11-13 21:19:45 -0500

    Bug fixes in the scale bar, actor settings adjusted
    It was showing the distance squared, may as well use new vtkVectors
    with their operators. The axis actor behaves better when it does not
    calculate "nice" numbers, as it hides quite significant shifts.

--- ScaleActorBehavior.cxx
+++ ScaleActorBehavior.cxx
@@ -20,55 +20,52 @@
 #include "pqView.h"
 #include "vtkAxisActor2D.h"
 #include "vtkCallbackCommand.h"
-#include "vtkMath.h"
 #include "vtkNew.h"
 #include "vtkPVRenderView.h"
 #include "vtkRenderer.h"
 #include "vtkSMRenderViewProxy.h"
+#include "vtkVector.h"
+#include "vtkVectorOperators.h"
 
 namespace TEM
 {
 static void UpdateScale(vtkObject *caller,
-  unsigned long vtkNotUsed(eventId), void *clientData, void *)
+  unsigned long, void *clientData, void *)
 {
   vtkAxisActor2D *axis = static_cast<vtkAxisActor2D*>(clientData);
   vtkRenderer *ren = static_cast<vtkRenderer *>(caller);
 
-  double pos1[3];
-  pos1[0] = axis->GetPoint1()[0];
-  pos1[1] = axis->GetPoint1()[1];
-  pos1[2] = 0.0;
-  double pos2[3];
-  pos2[0] = axis->GetPoint2()[0];
-  pos2[1] = axis->GetPoint2()[1];
-  pos2[2] = 0.0;
+  vtkVector3d pos1(axis->GetPoint1()[0], axis->GetPoint1()[1], 0);
+  vtkVector3d pos2(axis->GetPoint2()[0], axis->GetPoint2()[1], 0);
 
   ren->NormalizedViewportToView(pos1[0], pos1[1], pos1[2]);
   ren->NormalizedViewportToView(pos2[0], pos2[1], pos2[2]);
   ren->ViewToWorld(pos1[0], pos1[1], pos1[2]);
   ren->ViewToWorld(pos2[0], pos2[1], pos2[2]);
-  double distance = vtkMath::Distance2BetweenPoints(pos1, pos2);
+
+  double distance = vtkVector3d(pos2 - pos1).Norm();
 
   int scale = floor(log10(distance) - 0.7);
+  //cout << "Scale: " << scale << ", Distance: " << distance << endl;
   switch (scale)
     {
     case -7:
     case -8:
     case -9:
       axis->SetTitle("nm");
-      axis->SetRange(0, distance * 1000000000.0);
+      axis->SetRange(0, distance * 1e9);
       break;
     case -4:
     case -5:
     case -6:
       axis->SetTitle("microns");
-      axis->SetRange(0, distance * 1000000.0);
+      axis->SetRange(0, distance * 1e6);
       break;
     case -1:
     case -2:
     case -3:
       axis->SetTitle("mm");
-      axis->SetRange(0, distance * 1000.0);
+      axis->SetRange(0, distance * 1e3);
       break;
     case 2:
     case 1:
@@ -80,7 +77,7 @@ static void UpdateScale(vtkObject *caller,
     case 4:
     case 3:
       axis->SetTitle("km");
-      axis->SetRange(0, distance / 1000.0);
+      axis->SetRange(0, distance * 1e-3);
       break;
     default:
       axis->SetTitle("out of range");
@@ -112,12 +109,13 @@ void ScaleActorBehavior::viewAdded(pqView* view)
     Q_ASSERT(ren);
 
     vtkNew<vtkAxisActor2D> axis;
-    axis->SetPoint1(0.80, 0.1);
+    axis->SetPoint1(0.70, 0.1);
     axis->SetPoint2(0.95, 0.1);
     axis->SetTitle("m");
-    axis->SetLabelFormat("%3.0f");
+    axis->SetLabelFormat("%3.1f");
     axis->SetRulerMode(1);
     axis->SetNumberOfLabels(2);
+    axis->SetAdjustLabels(0);
 
     vtkNew<vtkCallbackCommand> cbc;
     cbc->SetCallback(UpdateScale);
