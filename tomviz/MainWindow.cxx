685e96fbd95b13a2af1d320cc1fcfde8cc1752e0
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2015-06-15 10:16:50 -0400

    Disable downsample/resample for now
    Needs work in order to avoid sampling from neighoring slices,
    disabling until this can be improved.

--- tomviz/MainWindow.cxx
+++ tomviz/MainWindow.cxx
@@ -150,7 +150,7 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
   //QAction *misalignGaussianAction = new QAction("Misalign (Gaussian)", this);
   QAction *squareRootAction = new QAction("Square Root Data", this);
   QAction *fftAbsLogAction = new QAction("FFT (abs log)", this);
-  QAction *resampleDataAction = new QAction("Clone && Downsample", this);
+  //QAction *resampleDataAction = new QAction("Clone && Downsample", this);
 
   ui.menuData->insertAction(ui.actionAlign, customPythonAction);
   ui.menuData->insertAction(ui.actionAlign, cropDataAction);
@@ -166,13 +166,13 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
   ui.menuData->insertAction(ui.actionClone, squareRootAction);
   ui.menuData->insertAction(ui.actionClone, fftAbsLogAction);
   ui.menuData->insertSeparator(ui.actionClone);
-  ui.menuData->insertAction(ui.actionClone, resampleDataAction);
+  //ui.menuData->insertAction(ui.actionClone, resampleDataAction);
 
   // Add our Python script reactions, these compose Python into menu entries.
   new AddExpressionReaction(customPythonAction);
   new AddPythonTransformReaction(cropDataAction,
                                  "Crop", Crop_Data);
-  new AddResampleReaction(resampleDataAction);
+  //new AddResampleReaction(resampleDataAction);
   //new AddPythonTransformReaction(backgroundSubtractAction,
   //                               "Background Subtraction",
   //                               Subtract_TiltSer_Background);
