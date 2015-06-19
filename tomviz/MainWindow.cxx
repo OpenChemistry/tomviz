28b5cc3a3ac7209722dd13ccfc84fdf12fb88801
Elliot Padgett <elliot.padgett@gmail.com>
2015-06-19 18:29:21 -0400

    Added Yi's auto find in-plane rotation python to menu

--- tomviz/MainWindow.cxx
+++ tomviz/MainWindow.cxx
@@ -51,6 +51,7 @@
 
 #include "MisalignImgs_Uniform.h"
 #include "Align_Images.h"
+#include "Axis_Rotation.h"
 #include "Recon_DFT.h"
 #include "DownsampleByTwo.h"
 #include "Crop_Data.h"
@@ -134,6 +135,7 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
    * ---
    * Manual Align
    * Auto Align (XCORR) - Align_Images.py
+   * Auto Find Axis Rotation -
    * Shift Uniformly - Shift_Stack_Uniformly.py
    * Misalign (Uniform) - MisalignImgs_Uniform.py
    * Misalign (Gaussian) - MisalignImgs_Gaussian.py
@@ -156,6 +158,7 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
   QAction *cropDataAction = new QAction("Crop", this);
   //QAction *backgroundSubtractAction = new QAction("Background Subtraction", this);
   QAction *autoAlignAction = new QAction("Auto Align (xcorr)", this);
+  QAction *autoAxisRotationAction = new QAction("Auto Find Axis Rotation", this);
   QAction *shiftUniformAction = new QAction("Shift Uniformly", this);
   QAction *downsampleByTwoAction = new QAction("Downsample x2", this);
   QAction *rotateAction = new QAction("Rotate", this);
@@ -172,6 +175,7 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
   //ui.menuData->insertAction(ui.actionAlign, backgroundSubtractAction);
   ui.menuData->insertSeparator(ui.actionAlign);
   ui.menuData->insertAction(ui.actionReconstruct, autoAlignAction);
+  ui.menuData->insertAction(ui.actionReconstruct, autoAxisRotationAction);
   ui.menuData->insertAction(ui.actionReconstruct, shiftUniformAction);
   ui.menuData->insertAction(ui.actionReconstruct, downsampleByTwoAction);
   ui.menuData->insertAction(ui.actionReconstruct, rotateAction);
@@ -195,6 +199,8 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
   ui.actionAlign->setText("Manual Align");
   new AddPythonTransformReaction(autoAlignAction,
                                  "Auto Align (XCORR)", Align_Images);
+  new AddPythonTransformReaction(autoAxisRotationAction,
+                                 "Auto Find Axis Rotation", Axis_Rotation);
   new AddPythonTransformReaction(shiftUniformAction,
                                  "Shift Uniformly", Shift_Stack_Uniformly);
   new AddPythonTransformReaction(downsampleByTwoAction,
