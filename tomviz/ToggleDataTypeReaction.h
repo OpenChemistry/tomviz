1247832e530551ed2ea22d63385cfb1a2f02ec92
Shawn Waldon <shawn.waldon@kitware.com>
2015-07-16 16:42:22 -0400

    Add menu item to toggle between volume and tilt series

---
+++ tomviz/ToggleDataTypeReaction.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+  This source file is part of the tomviz project.
+
+  Copyright Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+#ifndef tomvizToggleDataTypeReaction_h
+#define tomvizToggleDataTypeReaction_h
+
+#include "pqReaction.h"
+
+namespace tomviz
+{
+
+class DataSource;
+
+class ToggleDataTypeReaction : public pqReaction
+{
+  Q_OBJECT
+  typedef pqReaction Superclass;
+
+public:
+  ToggleDataTypeReaction(QAction* action);
+  ~ToggleDataTypeReaction();
+
+protected:
+  /// Called when the action is triggered.
+  virtual void onTriggered();
+  virtual void updateEnableState();
+
+private:
+  void setWidgetText(DataSource* dsource);
+
+  Q_DISABLE_COPY(ToggleDataTypeReaction)
+};
+
+}
+
+#endif
