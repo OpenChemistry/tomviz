c580c99200cace17f00a43a9b4e436df5a72218a
Yi Jiang <yijiang@Yis-MacBook-Pro-2.local>
2015-09-04 00:18:29 -0400

    Add a Sobel filter

---
+++ tomviz/python/SobelFilter.py
@@ -0,0 +1,16 @@
+def transform_scalars(dataset):
+    """Apply a Sobel filter to dataset."""
+    """Sobel Filter hgihtlights high intensity variations."""
+    
+    from tomviz import utils
+    import numpy as np
+    import scipy.ndimage
+
+    array = utils.get_array(dataset)
+
+    # transform the dataset
+    result = scipy.ndimage.filters.sobel(array)
+    
+    # set the result as the new scalars.
+    utils.set_array(dataset, result)
+    
