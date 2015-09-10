bbbb0f88156c871f7391577a40796c7bc94e008f
Shawn Waldon <shawn.waldon@kitware.com>
2015-09-10 17:26:54 -0400

    Fix indentation in OperatorPython.cxx

--- tomviz/OperatorPython.cxx
+++ tomviz/OperatorPython.cxx
@@ -32,66 +32,66 @@ namespace
 {
   // Smart pointer for PyObjects. Calls Py_XDECREF when scope ends.
   class SmartPyObject
-    {
+  {
     PyObject *Object;
 
   public:
     SmartPyObject(PyObject *obj = NULL)
       : Object(obj)
-      {
-      }
+    {
+    }
     ~SmartPyObject()
-      {
+    {
       Py_XDECREF(this->Object);
-      }
+    }
     PyObject *operator->() const
-      {
+    {
       return this->Object;
-      }
+    }
     PyObject *GetPointer() const
-      {
+    {
       return this->Object;
-      }
+    }
     operator bool () const
-      {
+    {
       return this->Object != NULL;
-      }
+    }
     operator PyObject* () const
-      {
+    {
       return this->Object;
-      }
+    }
 
     void TakeReference(PyObject* obj)
-      {
+    {
       if (this->Object)
-        {
-        Py_DECREF(this->Object);
-        }
-      this->Object = obj;
-      }
-    PyObject* ReleaseReference()
       {
+        Py_DECREF(this->Object);
+      }
+      this->Object = obj;
+    }
+    PyObject* ReleaseReference()
+    {
       PyObject* ret = this->Object;
       this->Object = NULL;
       return ret;
-      }
+    }
   private:
     SmartPyObject(const SmartPyObject&);
     void operator=(const SmartPyObject&) const;
-    };
+  };
 
   //----------------------------------------------------------------------------
   bool CheckForError()
-    {
+  {
     PyObject *exception = PyErr_Occurred();
     if (exception)
-      {
+    {
       PyErr_Print();
       PyErr_Clear();
       return true;
-      }
-    return false;
     }
+    return false;
+  }
 
   class EditPythonOperatorWidget : public tomviz::EditOperatorWidget
   {
@@ -145,10 +145,10 @@ OperatorPython::OperatorPython(QObject* parentObject) :
   vtkPythonInterpreter::Initialize();
   this->Internals->OperatorModule.TakeReference(PyImport_ImportModule("tomviz.utils"));
   if (!this->Internals->OperatorModule)
-    {
+  {
     qCritical() << "Failed to import tomviz.utils module.";
     CheckForError();
-    }
+  }
 }
 
 //-----------------------------------------------------------------------------
@@ -173,7 +173,7 @@ QIcon OperatorPython::icon() const
 void OperatorPython::setScript(const QString& str)
 {
   if (this->Script != str)
-    {
+  {
     this->Script = str;
     this->Internals->Code.TakeReference(NULL);
     this->Internals->TransformMethod.TakeReference(NULL);
@@ -183,34 +183,34 @@ void OperatorPython::setScript(const QString& str)
         this->label().toLatin1().data(),
         Py_file_input/*Py_eval_input*/));
     if (!this->Internals->Code)
-      {
+    {
       CheckForError();
       qCritical("Invalid script. Please check the traceback message for details");
       return;
-      }
+    }
 
     SmartPyObject module;
     module.TakeReference(PyImport_ExecCodeModule(
         QString("tomviz_%1").arg(this->label()).toLatin1().data(),
         this->Internals->Code));
     if (!module)
-      {
+    {
       CheckForError();
       qCritical("Failed to create module.");
       return;
-      }
+    }
 
     this->Internals->TransformMethod.TakeReference(
       PyObject_GetAttrString(module, "transform_scalars"));
     if (!this->Internals->TransformMethod)
-      {
+    {
       CheckForError();
       qWarning("Script doesn't have any 'transform' function.");
       return;
-      }
+    }
     CheckForError();
     emit this->transformModified();
-    }
+  }
 }
 
 //-----------------------------------------------------------------------------
@@ -218,9 +218,9 @@ bool OperatorPython::transform(vtkDataObject* data)
 {
   if (this->Script.isEmpty()) { return true; }
   if (!this->Internals->OperatorModule || !this->Internals->TransformMethod)
-    {
+  {
     return true;
-    }
+  }
 
   Q_ASSERT(data);
 
@@ -232,11 +232,11 @@ bool OperatorPython::transform(vtkDataObject* data)
   result.TakeReference(PyObject_Call(this->Internals->TransformMethod, args,
                                      NULL));
   if (!result)
-    {
+  {
     qCritical("Failed to execute the script.");
     CheckForError();
     return false;
-    }
+  }
 
   return CheckForError() == false;
 }
