a71cc6cf86c7dfc9482fb436bc651720460a5830
Lijie Tu <Lijie@dhcp-ccc-296.eduroam.cornell.edu>
2015-11-20 15:17:50 -0500

    Gaussian filter added

---
+++ tomviz/python/GaussianFilter.py
@@ -0,0 +1,20 @@
+def transform_scalars(dataset):
+    """Apply a Gaussian filter to dataset."""
+    """Gaussian Filter blurs the image and reduces the noise and details."""
+    
+    #----USER SPECIFIED VARIABLES-----#
+    ###Filter_AXIS###    #Specify sigma of the Gaussian Function
+    #---------------------------------#
+
+    from tomviz import utils
+    import numpy as np
+    import scipy.ndimage
+
+    array = utils.get_array(dataset)
+
+    # transform the dataset
+    result = scipy.ndimage.filters.gaussian_filter(array,sigma=2.0)
+    
+    # set the result as the new scalars.
+    utils.set_array(dataset, result)
+    
