fab4a760fc22c84e89b302e293ab2dd4a0cbcd80
Shawn Waldon <shawn.waldon@kitware.com>
2015-11-25 14:49:34 -0500

    Fix indentation in reconstruction python files
    On some platforms, python complains if the indentation is not consistent

--- tomviz/python/Recon_WBP.py
+++ tomviz/python/Recon_WBP.py
@@ -35,12 +35,12 @@ def wbp3(input,angles,N=None,filter="ramp",interp="linear"):
     input = np.double(input)
     (Nslice,Nray,Nproj) = input.shape
     if Nproj!=angles.size:
-       raise ValueError('Data does not match angles!')
+        raise ValueError('Data does not match angles!')
     interpolation_methods = ('linear','nearest','spline','cubic')
     if not interp in interpolation_methods:
-       raise ValueError("Unknown interpolation: %s" % interp)
+        raise ValueError("Unknown interpolation: %s" % interp)
     if not N: #if ouput size is not given
-       N = int(np.floor(np.sqrt(Nray**2/2.0)))
+        N = int(np.floor(np.sqrt(Nray**2/2.0)))
     
     recon = np.zeros((Nslice,N,N))
     for i in range(Nslice):
@@ -63,16 +63,16 @@ def wbp2(sinogram,angles,N=None,filter="ramp",interp="linear"):
     #print "Angles size is:", angles.shape
     #check inputs
     if sinogram.ndim!=2:
-       raise ValueError('Sinogram must be 2D')
+        raise ValueError('Sinogram must be 2D')
     (Nray,Nproj) = sinogram.shape
     if Nproj!=angles.size:
-       raise ValueError('Sinogram does not match angles!')
+        raise ValueError('Sinogram does not match angles!')
  
     interpolation_methods = ('linear','nearest','spline','cubic')
     if not interp in interpolation_methods:
-       raise ValueError("Unknown interpolation: %s" % interp)
+        raise ValueError("Unknown interpolation: %s" % interp)
     if not N: #if ouput size is not given
-       N = int(np.floor(np.sqrt(Nray**2/2.0)))
+        N = int(np.floor(np.sqrt(Nray**2/2.0)))
     
     ang = np.double(angles)*np.pi/180.0
     #create Fourier filter
@@ -96,13 +96,13 @@ def wbp2(sinogram,angles,N=None,filter="ramp",interp="linear"):
         t = ypr * np.cos(ang[j]) - xpr * np.sin(ang[j])
         x = np.arange(Nray) - center_proj
         if interp == 'linear': 
-	   bp = np.interp(t,x,s[:,j],left=0,right=0)
+            bp = np.interp(t,x,s[:,j],left=0,right=0)
         elif interp == 'spline':
-           interpolant = interp1d(x,s[:,j],kind='slinear',bounds_error=False,fill_value=0)
-           bp = interpolant(t)
+            interpolant = interp1d(x,s[:,j],kind='slinear',bounds_error=False,fill_value=0)
+            bp = interpolant(t)
         else:
-           interpolant = interp1d(x,s[:,j],kind=interp,bounds_error=False,fill_value=0)
-           bp = interpolant(t)
+            interpolant = interp1d(x,s[:,j],kind=interp,bounds_error=False,fill_value=0)
+            bp = interpolant(t)
         recon = recon + bp
     
     #normalize
@@ -120,19 +120,19 @@ def makeFilter(Nray,filterMethod="ramp"):
     filter = 2 * np.abs(freq)
 
     if filterMethod=="ramp":
-       pass
+        pass
     elif filterMethod=="shepp-logan":
-         filter[1:] = filter[1:] * np.sin(omega[1:]) / omega[1:]
+        filter[1:] = filter[1:] * np.sin(omega[1:]) / omega[1:]
     elif filterMethod=="cosine":
-         filter[1:] = filter[1:] * np.cos(filter[1:])
+        filter[1:] = filter[1:] * np.cos(filter[1:])
     elif filterMethod=="hamming":
-         filter[1:] = filter[1:] * (0.54 + 0.46*np.cos(omega[1:]/2) )
+        filter[1:] = filter[1:] * (0.54 + 0.46*np.cos(omega[1:]/2) )
     elif filterMethod=="hann":
-         filter[1:] = filter[1:] * (1 + np.cos(omega[1:]/2) ) / 2
+        filter[1:] = filter[1:] * (1 + np.cos(omega[1:]/2) ) / 2
     elif filterMethod=="none":
-         filter[:] = 1
+        filter[:] = 1
     else:
-         raise ValueError("Unknown filter: %s" % filterMethod)
+        raise ValueError("Unknown filter: %s" % filterMethod)
 
     return filter
     
