b3b6c319f0b036a1b5d82474d1c5c94368f26872
Lijie Tu <Lijie@dhcp-ccc-4116.eduroam.cornell.edu>
2015-11-27 17:34:05 -0500

    add median filter

--- tomviz/AddPythonTransformReaction.cxx
+++ tomviz/AddPythonTransformReaction.cxx
@@ -449,6 +449,45 @@ OperatorPython* AddPythonTransformReaction::addExpression(DataSource* source)
        addPythonOperator(source, this->scriptLabel, this->scriptSource, substitutions);
      }
   }
+    
+    
+  else if (scriptLabel == "Median Filter") //UI for Median Filter
+  {
+     QDialog dialog(pqCoreUtilities::mainWidget());
+     dialog.setWindowTitle("Apply Median filter");
+     QGridLayout *layout = new QGridLayout;
+     QLabel *labelDescription = new QLabel("Apply an isotropic Median filter. \nThe size can be specidied below:");
+     layout->addWidget(labelDescription,0,0,1,2);
+    
+     QLabel *label = new QLabel("Size:", &dialog);
+     layout->addWidget(label);
+     QDoubleSpinBox *size = new QDoubleSpinBox(&dialog);
+     median->setSingleStep(0.5);
+     //median->setRange(0, 20);
+     median->setValue(2);
+     layout->addWidget(label,1,0,1,1);
+     layout->addWidget(median,1,1,1,1);
+    
+     QVBoxLayout *v = new QVBoxLayout;
+     QDialogButtonBox *buttons = new QDialogButtonBox(QDialogButtonBox::Ok
+                                                     | QDialogButtonBox::Cancel,
+                                                     Qt::Horizontal,
+                                                     &dialog);
+     connect(buttons, SIGNAL(accepted()), &dialog, SLOT(accept()));
+     connect(buttons, SIGNAL(rejected()), &dialog, SLOT(reject()));
+     v->addLayout(layout);
+     v->addWidget(buttons);
+     dialog.setLayout(v);
+     dialog.layout()->setSizeConstraint(QLayout::SetFixedSize); //Make the UI non-resizeable
+     if (dialog.exec() == QDialog::Accepted)
+     {
+       QMap<QString, QString> substitutions;
+       substitutions.insert("###Median###",
+                             QString("median = %1").arg(size->value()) );
+       addPythonOperator(source, this->scriptLabel, this->scriptSource, substitutions);
+     }
+  }
+
 
   else if (scriptLabel == "Resample")
   {
---
+++ tomviz/python/MedianFilter.py
@@ -0,0 +1,20 @@
+def transform_scalars(dataset):
+    """Apply a Median filter to dataset."""
+    """ Median filter is a nonlinear filter used to reduce noise."""
+    
+    #----USER SPECIFIED VARIABLES-----#
+    ###Size###    #Specify size of the Gaussian Function
+    #---------------------------------#
+
+    from tomviz import utils
+    import numpy as np
+    import scipy.ndimage
+
+    array = utils.get_array(dataset)
+
+    # transform the dataset
+    result = scipy.ndimage.filters.median_filter(array,sigma)
+    
+    # set the result as the new scalars.
+    utils.set_array(dataset, result)
+    
