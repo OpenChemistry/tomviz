eb4cd062886f4c8debc7afa364bd349d1ab18bee
Yi Jiang <yj245@cornell.edu>
2015-12-04 09:59:08 -0500

    Clean up python script

--- tomviz/python/SobelFilter.py
+++ tomviz/python/SobelFilter.py
@@ -1,6 +1,6 @@
 def transform_scalars(dataset):
-    """Calaulate 3D Sobel filter of dataset."""
-    """Sobel Filter hgihlights high intensity variations."""
+    """Apply 3D Sobel filter to dataset"""
+    """Sobel Filter hgihlights high intensity variations"""
 
     from tomviz import utils
     import scipy.ndimage
@@ -8,7 +8,7 @@ def transform_scalars(dataset):
     array = utils.get_array(dataset)
 
     # transform the dataset
-    #result = scipy.ndimage.filters.sobel(array,axis=-Filter_AXIS)
     result = scipy.ndimage.filters.generic_gradient_magnitude(array, scipy.ndimage.filters.sobel)
+    
     # set the result as the new scalars.
     utils.set_array(dataset, result)
