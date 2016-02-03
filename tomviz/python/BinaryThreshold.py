aaa78a663761a6d3df6061d813ab9e82594c0175
Cory Quammen <cory.quammen@kitware.com>
2016-02-03 11:46:59 -0500

    Updated Binary Threshold filter to newer ITK/VTK style

--- tomviz/python/BinaryThreshold.py
+++ tomviz/python/BinaryThreshold.py
@@ -6,6 +6,7 @@ def transform_scalars(dataset):
     try:
         import itk
         import vtk
+        from tomviz import utils
     except Exception as exc:
         print("Could not import necessary module(s)")
         print(exc)
@@ -14,25 +15,20 @@ def transform_scalars(dataset):
     lower_threshold = 40
     upper_threshold = 255
 
-    # Add a try/catch around the ITK portion. ITK exceptions are
+    # Add a try/except around the ITK portion. ITK exceptions are
     # passed up to the Python layer, so we can at least report what
     # went wrong with the script, e.g,, unsupported image type.
-
     try:
-        # Set up some ITK variables
-        itk_input_image_type = itk.Image.UC3
-        itk_import = itk.VTKImageToImageFilter[itk_input_image_type].New()
-        itk_import.SetInput(dataset)
-        itk_import.Update()
+        # Get the ITK image
+        itk_image = utils.convert_vtk_to_itk_image(dataset)
+        itk_input_image_type = type(itk_image)
 
-        # Read the image into ITK
-        itk_image = itk_import.GetOutput()
-
-        # We change the output type to unsigned char 3D (itk.Image.UC3D) to save memory
-        # in the output label map representation
+        # We change the output type to unsigned char 3D
+        # (itk.Image.UC3D) to save memory in the output label map
+        # representation.
         itk_output_image_type = itk.Image.UC3
 
-        # ITK's BinaryThresholdImageFilter
+        # ITK's BinaryThresholdImageFilter does the hard work
         threshold_filter = itk.BinaryThresholdImageFilter[itk_input_image_type,itk_output_image_type].New()
         threshold_filter.SetLowerThreshold(lower_threshold)
         threshold_filter.SetUpperThreshold(upper_threshold)
@@ -41,23 +37,7 @@ def transform_scalars(dataset):
         threshold_filter.SetInput(itk_image)
         threshold_filter.Update()
 
-        # Export the ITK image to a VTK image. No copying should take place.
-        export_filter = itk.ImageToVTKImageFilter[itk_output_image_type].New()
-        export_filter.SetInput(threshold_filter.GetOutput())
-        export_filter.Update()
-
-        # Get scalars from the temporary image and copy them to the data set
-        result_image = export_filter.GetOutput()
-        filter_array = result_image.GetPointData().GetArray(0)
-
-        # Make a new instance of the array that will stick around after this
-        # filters in this script are garbage collected
-        label_map = filter_array.NewInstance()
-        label_map.DeepCopy(filter_array) # Should be able to shallow copy?
-        label_map.SetName('LabelMap')
-
-        # Set a new point data array in the dataset
-        dataset.GetPointData().AddArray(label_map)
+        utils.add_vtk_array_from_itk_image(threshold_filter.GetOutput(), dataset, 'LabelMap')
 
     except Exception as exc:
         print("Exception encountered while running BinaryThreshold");
