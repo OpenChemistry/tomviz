3ccfd67c0a8e9df727f45f0fb5491b3713f2a9d7
Yi Jiang <yj245@cornell.edu>
2016-06-29 17:47:48 -0400

    switched to pyfftw

--- tomviz/python/Recon_DFT.py
+++ tomviz/python/Recon_DFT.py
@@ -20,7 +20,9 @@ def transform_scalars(dataset):
     # Mark dataset as volume
     utils.mark_as_volume(dataset)
 
+import pyfftw
 import numpy as np
+
 def dfm3(input,angles,Npad):
     # input: aligned data
     # angles: projection angles
@@ -35,38 +37,45 @@ def dfm3(input,angles,Npad):
     pad_post = np.ceil((Npad-Ny)/2); pad_pre = np.floor((Npad-Ny)/2)    
     # Initialize value and weight matrix.
     Nz = Ny
-    w = np.zeros((Nx,Ny,Nz))
-    v = np.zeros((Nx,Ny,Nz)) + 1j*np.zeros((Nx,Ny,Nz))
+    
+    w = np.zeros((Nx,Ny,np.int(Nz/2+1)))
+    v = pyfftw.n_byte_align_empty((Nx,Ny,Nz/2+1),16,dtype='complex128')
+    v = np.zeros(v.shape) + 1j*np.zeros(v.shape)
+    recon = pyfftw.n_byte_align_empty((Nx,Ny,Nz),16,dtype='float64')
+    recon_fftw_object = pyfftw.FFTW(v,recon,direction='FFTW_BACKWARD',axes=(0,1,2))
+
+    p = pyfftw.n_byte_align_empty((Nx,Npad),16,dtype='float64')
+    pF = pyfftw.n_byte_align_empty((Nx,Npad/2+1),16,dtype='complex128')
+    p_fftw_object = pyfftw.FFTW(p,pF,axes=(0,1))
 
     dk = np.double(Ny)/np.double(Npad) * 1
-
+    
     for a in range(0, Nproj):
         #print angles[a]
         ang = angles[a]*np.pi/180
         projection = input[:,:,a] #projection
         p = np.lib.pad(projection,((0,0),(pad_pre,pad_post)),'constant',constant_values=(0,0)) #pad zeros
         p = np.fft.ifftshift(p) 
-        pF = np.fft.fft2(p)
-        pF = np.fft.fftshift(pF)
+        p_fftw_object.update_arrays(p,pF) #FFTw
+        p_fftw_object()
 
         # Bilinear extrapolation
-        for i in range(0, np.int(Npad)):
-            ky = (i-cen_pad)*dk;  #kz = 0;
+        for i in range(0, np.int(np.ceil(Npad/2))+1):
+            ky = i*dk;  #kz = 0;
             ky_new = np.cos(ang)*ky #new coord. after rotation
             kz_new = np.sin(ang)*ky 
             sy = abs(np.floor(ky_new) - ky_new) #calculate weights
             sz = abs(np.floor(kz_new) - kz_new)
             for b in range(1,5): #bilinear extrapolation
                 pz,py,weight = bilinear(kz_new,ky_new,sz,sy,Ny,b)
-                pz = pz + cen;
-                py = py + cen;
-                if (py>=0 and py<Ny and pz>=0 and pz<Nz):
+                if (py>=0 and py<Ny and pz>=0 and pz<Nz/2+1):
                     w[:,py,pz] = w[:,py,pz] + weight
                     v[:,py,pz] = v[:,py,pz] + weight * pF[:,i]
 
     v[w!=0] = v[w!=0]/w[w!=0]
-    v = np.fft.ifftshift(v)
-    recon = np.real(np.fft.ifftn(v))
+    recon_F = v.copy()
+    recon_fftw_object.update_arrays(v,recon) #FFTw
+    recon_fftw_object()
     recon = np.fft.fftshift(recon)
     return recon.astype(np.float32)
 
