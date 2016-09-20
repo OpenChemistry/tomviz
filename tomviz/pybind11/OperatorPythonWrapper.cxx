58ae748a6fc7ec98d0118a6197eb7944e54a6068
Chris Harris <chris.harris@kitware.com>
2016-09-19 21:25:04 -0400

    Use pybind11 to generate Operator wrapper

---
+++ tomviz/pybind11/OperatorPythonWrapper.cxx
@@ -0,0 +1,28 @@
+#include <pybind11/pybind11.h>
+
+#include "OperatorPython.h"
+
+using namespace tomviz;
+
+struct OperatorPythonWrapper {
+  OperatorPythonWrapper(OperatorPython *o) { this->op = o; };
+  bool canceled() { return this->op->canceled(); }
+  OperatorPython *op;
+
+};
+
+namespace py = pybind11;
+
+PYBIND11_PLUGIN(_tomviz) {
+    py::module m("_tomviz", "tomviz wrapped classes");
+
+    py::class_<OperatorPythonWrapper>(m, "OperatorPythonWrapper")
+        .def("__init__",
+            [](OperatorPythonWrapper &instance, void *op) {
+                new (&instance) OperatorPythonWrapper(static_cast<OperatorPython*>(op));
+            }
+        )
+        .def_property_readonly("canceled", &OperatorPythonWrapper::canceled);
+
+    return m.ptr();
+}
