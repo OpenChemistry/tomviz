0ef962583092ffb730a97f51d0645550697af6cd
Chris Harris <chris.harris@kitware.com>
2016-09-26 09:56:15 -0400

    Add python copyright headers

--- tomviz/python/tomviz/_internal.py
+++ tomviz/python/tomviz/_internal.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+#  This source file is part of the tomviz project.
+#
+#  Copyright Kitware, Inc.
+#
+#  This source code is released under the New BSD License, (the "License").
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+###############################################################################
+
 import tomviz.operators
 import tomviz._wrapping
 import inspect
