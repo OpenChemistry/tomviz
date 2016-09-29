65f2aeece7d83a5a5357a733cfcc878bf59f7ff6
Chris Harris <chris.harris@kitware.com>
2016-09-29 16:26:04 -0400

    Add noqa for complex expressions
    These need reformating but are out of scope of this branch.

--- tomviz/python/Recon_TV_minimization.py
+++ tomviz/python/Recon_TV_minimization.py
@@ -82,20 +82,20 @@ def tv_minimization(A, tiltSeries, recon, iterNum=1):
 
             v2n = r - np.roll(r, -1, axis=0)
             v2d = np.sqrt(1e-8 + (np.roll(r, -1, axis=0) - r)**2
-                          + (np.roll(r, -1, axis=0) - \
+                          + (np.roll(r, -1, axis=0) - \ # noqa TODO reformat this
                              np.roll(np.roll(r, -1, axis=0), 1, axis=1))**2
-                          + (np.roll(r, -1, axis=0) - np.roll(np.roll(r, -1, axis=0), 1, axis=2))**2)
+                          + (np.roll(r, -1, axis=0) - np.roll(np.roll(r, -1, axis=0), 1, axis=2))**2) # noqa TODO reformat this
 
             v3n = r - np.roll(r, -1, axis=1)
-            v3d = np.sqrt(1e-8 + (np.roll(r, -1, axis=1) - np.roll(np.roll(r, -1, axis=1), 1, axis=0))**2
-                          + (np.roll(r, -1, axis=1) - r)**2
-                          + (np.roll(r, -1, axis=1) - np.roll(np.roll(r, -1, axis=1), 1, axis=2))**2)
+            v3d = np.sqrt(1e-8 + (np.roll(r, -1, axis=1) - np.roll(np.roll(r, -1, axis=1), 1, axis=0))**2 # noqa TODO reformat this
+                          + (np.roll(r, -1, axis=1) - r)**2 # noqa TODO reformat this
+                          + (np.roll(r, -1, axis=1) - np.roll(np.roll(r, -1, axis=1), 1, axis=2))**2) # noqa TODO reformat this
 
             v4n = r - np.roll(r, -1, axis=2)
-            v4d = np.sqrt(1e-8 + (np.roll(r, -1, axis=2) - np.roll(np.roll(r, -1, axis=2), 1, axis=0))**2
-                          + (np.roll(r, -1, axis=2) - \
+            v4d = np.sqrt(1e-8 + (np.roll(r, -1, axis=2) - np.roll(np.roll(r, -1, axis=2), 1, axis=0))**2 # noqa TODO reformat this
+                          + (np.roll(r, -1, axis=2) - \ # noqa TODO reformat this
                              np.roll(np.roll(r, -1, axis=1), 1, axis=1))**2
-                          + (np.roll(r, -1, axis=2) - r)**2)
+                          + (np.roll(r, -1, axis=2) - r)**2) # noqa TODO reformat this
 
             v = v1n / v1d + v2n / v2d + v3n / v3d + v4n / v4d
             v = v[1:-1, 1:-1, 1:-1]
