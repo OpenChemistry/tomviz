325fd632a4895c30a2869f25d9afe7f29064aa41
Cory Quammen <cory.quammen@kitware.com>
2016-09-30 08:56:33 -0400

    Added class for building a UI from a JSON plugin description

---
+++ tomviz/InterfaceBuilder.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+  This source file is part of the tomviz project.
+
+  Copyright Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+#ifndef tomvizInterfaceBuilder_h
+#define tomvizInterfaceBuilder_h
+
+#include <QLayout>
+#include <QObject>
+
+namespace tomviz {
+
+/// The InterfaceBuilder creates a Qt widget containing controls defined
+/// by a JSON description.
+class InterfaceBuilder : public QObject
+{
+  Q_OBJECT
+  typedef QObject Superclass;
+
+public:
+  InterfaceBuilder(QObject* parent = nullptr);
+  ~InterfaceBuilder() override;
+
+  /// Set the JSON description
+  void setJSONDescription(const QString& description);
+
+
+  /// Get the JSON description
+  const QString& JSONDescription() const;
+
+  /// Build the interface, returning it in a QWidget.
+  QLayout* buildInterface() const;
+
+private:
+  Q_DISABLE_COPY(InterfaceBuilder)
+
+  QString m_json;
+};
+
+} // namespace tomviz
+
+#endif
