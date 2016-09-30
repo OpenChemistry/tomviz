df1c61ea4a849635c15bf7c1d61ce74d5f073f8d
Cory Quammen <cory.quammen@kitware.com>
2016-09-30 14:11:33 -0400

    Convert Recon_ART to use JSON description

--- tomviz/MainWindow.cxx
+++ tomviz/MainWindow.cxx
@@ -281,7 +281,8 @@ MainWindow::MainWindow(QWidget* _parent, Qt::WindowFlags _flags)
                                  "Reconstruct (Back Projection)",
                                  readInPythonScript("Recon_WBP"), true);
   new AddPythonTransformReaction(reconARTAction, "Reconstruct (ART)",
-                                 readInPythonScript("Recon_ART"), true);
+                                 readInPythonScript("Recon_ART"), true, false,
+                                 readInJSONDescription("Recon_ART"));
   new AddPythonTransformReaction(reconSIRTAction, "Reconstruct (SIRT)",
                                  readInPythonScript("Recon_SIRT"), true);
   new AddPythonTransformReaction(
