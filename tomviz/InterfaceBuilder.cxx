32b3373ca71da63facbe12c341c1572a621d7206
Cory Quammen <cory.quammen@kitware.com>
2016-10-05 21:12:39 -0400

    Use templates to reduce code redundancy

--- tomviz/InterfaceBuilder.cxx
+++ tomviz/InterfaceBuilder.cxx
@@ -36,6 +36,85 @@
 
 namespace {
 
+// Templated query of Json::Value type
+template <typename T>
+bool isType(const Json::Value&)
+{
+  return false;
+}
+
+template <>
+bool isType<int>(const Json::Value& value)
+{
+  return value.isInt();
+}
+
+template <>
+bool isType<double>(const Json::Value& value)
+{
+  return value.isDouble();
+}
+
+// Templated accessor of Json::Value value
+template <typename T>
+T getAs(const Json::Value&)
+{
+  return 0;
+}
+
+template <>
+int getAs(const Json::Value& value)
+{
+  int iValue = 0;
+  try {
+    iValue = value.asInt();
+  } catch (...) {
+    qCritical() << "Could not get int from Json::Value";
+  }
+  return value.asInt();
+}
+
+template <>
+double getAs(const Json::Value& value)
+{
+  double dValue = 0.0;
+  try {
+    dValue = value.asDouble();
+  } catch (...) {
+    qCritical() << "Could not get double from Json::Value";
+  }
+  return dValue;
+}
+
+// Templated generation of numeric editing widgets.
+template <typename T>
+QWidget* getNumericWidget(T, T, T)
+{
+  return nullptr;
+}
+
+template <>
+QWidget* getNumericWidget(int defaultValue, int rangeMin, int rangeMax)
+{
+  tomviz::SpinBox* spinBox = new tomviz::SpinBox();
+  spinBox->setSingleStep(1);
+  spinBox->setMinimum(rangeMin);
+  spinBox->setMaximum(rangeMax);
+  spinBox->setValue(defaultValue);
+  return spinBox;
+}
+
+template <>
+QWidget* getNumericWidget(double defaultValue, double rangeMin, double rangeMax)
+{
+  tomviz::DoubleSpinBox* spinBox = new tomviz::DoubleSpinBox();
+  spinBox->setSingleStep(0.5);
+  spinBox->setMinimum(rangeMin);
+  spinBox->setMaximum(rangeMax);
+  spinBox->setValue(defaultValue);
+  return spinBox;
+}
+
 void addBoolWidget(QGridLayout* layout, int row,
                    const Json::Value& parameterNode)
 {
@@ -67,8 +146,9 @@ void addBoolWidget(QGridLayout* layout, int row,
   layout->addWidget(checkBox, row, 1, 1, 1);
 }
 
-void addIntWidget(QGridLayout* layout, int row,
-                  const Json::Value& parameterNode)
+template <typename T>
+void addNumericWidget(QGridLayout* layout, int row,
+                      const Json::Value& parameterNode)
 {
   Json::Value nameValue = parameterNode["name"];
   Json::Value labelValue = parameterNode["label"];
@@ -85,37 +165,35 @@ void addIntWidget(QGridLayout* layout, int row,
   }
   layout->addWidget(label, row, 0, 1, 1);
 
-  std::vector<int> defaultValues;
+  std::vector<T> defaultValues;
   if (parameterNode.isMember("default")) {
     Json::Value defaultNode = parameterNode["default"];
-    if (defaultNode.isInt()) {
-      defaultValues.push_back(defaultNode.asInt());
+    if (isType<T>(defaultNode)) {
+      defaultValues.push_back(getAs<T>(defaultNode));
     } else if (defaultNode.isArray()) {
       for (Json::Value::ArrayIndex i = 0; i < defaultNode.size(); ++i) {
-        defaultValues.push_back(defaultNode[i].asInt());
+        defaultValues.push_back(getAs<T>(defaultNode[i]));
       }
     }
   }
 
-  std::vector<int> minValues(defaultValues.size(),
-                             std::numeric_limits<int>::min());
+  std::vector<T> minValues(defaultValues.size(), std::numeric_limits<T>::min());
   Json::Value minNode = parameterNode["minimum"];
-  if (minNode.isInt()) {
-    minValues[0] = minNode.asInt();
+  if (isType<T>(minNode)) {
+    minValues[0] = getAs<T>(minNode);
   } else if (minNode.isArray()) {
     for (Json::Value::ArrayIndex i = 0; i < minNode.size(); ++i) {
-      minValues[i] = minNode[i].asInt();
+      minValues[i] = getAs<T>(minNode[i]);
     }
   }
 
-  std::vector<int> maxValues(defaultValues.size(),
-                             std::numeric_limits<int>::max());
+  std::vector<T> maxValues(defaultValues.size(), std::numeric_limits<T>::max());
   Json::Value maxNode = parameterNode["maximum"];
-  if (maxNode.isInt()) {
-    maxValues[0] = maxNode.asInt();
+  if (isType<T>(maxNode)) {
+    maxValues[0] = getAs<T>(maxNode);
   } else if (maxNode.isArray()) {
     for (Json::Value::ArrayIndex i = 0; i < maxNode.size(); ++i) {
-      maxValues[i] = maxNode[i].asInt();
+      maxValues[i] = getAs<T>(maxNode[i]);
     }
   }
 
@@ -134,89 +212,10 @@ void addIntWidget(QGridLayout* layout, int row,
       name.append("#%1");
       name = name.arg(i, 3, 10, QLatin1Char('0'));
     }
-    tomviz::SpinBox* spinBox = new tomviz::SpinBox();
+
+    QWidget* spinBox =
+      getNumericWidget(defaultValues[i], minValues[i], maxValues[i]);
     spinBox->setObjectName(name);
-    spinBox->setSingleStep(1);
-    spinBox->setMinimum(minValues[i]);
-    spinBox->setMaximum(maxValues[i]);
-    spinBox->setValue(defaultValues[i]);
-    horizontalLayout->addWidget(spinBox);
-  }
-}
-
-void addDoubleWidget(QGridLayout* layout, int row,
-                     const Json::Value& parameterNode)
-{
-  Json::Value nameValue = parameterNode["name"];
-  Json::Value labelValue = parameterNode["label"];
-
-  if (nameValue.isNull()) {
-    qWarning() << QString("Parameter %1 has no name. Skipping.")
-                    .arg(parameterNode.toStyledString().c_str());
-    return;
-  }
-
-  QLabel* label = new QLabel(nameValue.asCString());
-  if (!labelValue.isNull()) {
-    label->setText(labelValue.asCString());
-  }
-  layout->addWidget(label, row, 0, 1, 1);
-
-  std::vector<double> defaultValues;
-  if (parameterNode.isMember("default")) {
-    Json::Value defaultNode = parameterNode["default"];
-    if (defaultNode.isDouble()) {
-      defaultValues.push_back(defaultNode.asDouble());
-    } else if (defaultNode.isArray()) {
-      for (Json::Value::ArrayIndex i = 0; i < defaultNode.size(); ++i) {
-        defaultValues.push_back(defaultNode[i].asDouble());
-      }
-    }
-  }
-
-  std::vector<double> minValues(defaultValues.size(),
-                                std::numeric_limits<double>::min());
-  Json::Value minNode = parameterNode["minimum"];
-  if (minNode.isDouble()) {
-    minValues[0] = minNode.asDouble();
-  } else if (minNode.isArray()) {
-    for (Json::Value::ArrayIndex i = 0; i < minNode.size(); ++i) {
-      minValues[i] = minNode[i].asDouble();
-    }
-  }
-
-  std::vector<double> maxValues(defaultValues.size(),
-                                std::numeric_limits<double>::max());
-  Json::Value maxNode = parameterNode["maximum"];
-  if (maxNode.isDouble()) {
-    maxValues[0] = maxNode.asInt();
-  } else if (maxNode.isArray()) {
-    for (Json::Value::ArrayIndex i = 0; i < maxNode.size(); ++i) {
-      maxValues[i] = maxNode[i].asDouble();
-    }
-  }
-
-  QHBoxLayout* horizontalLayout = new QHBoxLayout();
-  horizontalLayout->setContentsMargins(0, 0, 0, 0);
-  QWidget* horizontalWidget = new QWidget;
-  horizontalWidget->setLayout(horizontalLayout);
-  layout->addWidget(horizontalWidget, row, 1, 1, 1);
-
-  for (size_t i = 0; i < defaultValues.size(); ++i) {
-    QString name(nameValue.asCString());
-    if (defaultValues.size() > 1) {
-      // Multi-element parameters are named with the pattern 'basename#XXX'
-      // where 'basename' is the name of the parameter and 'XXX' is the
-      // element number.
-      name.append("#%1");
-      name = name.arg(i, 3, 10, QLatin1Char('0'));
-    }
-    tomviz::DoubleSpinBox* spinBox = new tomviz::DoubleSpinBox();
-    spinBox->setObjectName(name);
-    spinBox->setSingleStep(0.5);
-    spinBox->setMinimum(minValues[i]);
-    spinBox->setMaximum(maxValues[i]);
-    spinBox->setValue(defaultValues[i]);
     horizontalLayout->addWidget(spinBox);
   }
 }
@@ -268,8 +267,7 @@ void addEnumerationWidget(QGridLayout* layout, int row,
   layout->addWidget(comboBox, row, 1, 1, 1);
 }
 
-void addXYZHeaderWidget(QGridLayout* layout, int row,
-                        const Json::Value&)
+void addXYZHeaderWidget(QGridLayout* layout, int row, const Json::Value&)
 {
   QHBoxLayout* horizontalLayout = new QHBoxLayout;
   horizontalLayout->setContentsMargins(0, 0, 0, 0);
@@ -358,12 +356,12 @@ QLayout* InterfaceBuilder::buildInterface() const
     if (typeString == "bool") {
       addBoolWidget(layout, i + 1, parameterNode);
     } else if (typeString == "int") {
-      addIntWidget(layout, i + 1, parameterNode);
+      addNumericWidget<int>(layout, i + 1, parameterNode);
     } else if (typeString == "double") {
-      addDoubleWidget(layout, i + 1, parameterNode);
+      addNumericWidget<double>(layout, i + 1, parameterNode);
     } else if (typeString == "enumeration") {
       addEnumerationWidget(layout, i + 1, parameterNode);
-    } else if (typeString =="xyz_header") {
+    } else if (typeString == "xyz_header") {
       addXYZHeaderWidget(layout, i + 1, parameterNode);
     }
   }
