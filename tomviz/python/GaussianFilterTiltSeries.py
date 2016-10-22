083bf5c89b9ac698b4dfe989869f2ef581ca5285
Yi Jiang <yj245@cornell.edu>
2016-10-22 17:00:30 -0400

    fix python formatting

--- tomviz/python/GaussianFilterTiltSeries.py
+++ tomviz/python/GaussianFilterTiltSeries.py
@@ -3,7 +3,7 @@ def transform_scalars(dataset):
     """Gaussian Filter blurs the image and reduces the noise and details."""
 
     #----USER SPECIFIED VARIABLES-----#
-    ###sigma###    #Specify sigma of the Gaussian Function
+    # sigma###    #Specify sigma of the Gaussian Function
     #---------------------------------#
 
     from tomviz import utils
@@ -12,7 +12,8 @@ def transform_scalars(dataset):
     tiltSeries = utils.get_array(dataset)
 
     # Transform the dataset.
-    result = scipy.ndimage.filters.gaussian_filter(tiltSeries, [sigma, sigma, 0])
+    result = scipy.ndimage.filters.gaussian_filter(
+        tiltSeries, [sigma, sigma, 0])
 
     # Set the result as the new scalars.
     utils.set_array(dataset, result)
