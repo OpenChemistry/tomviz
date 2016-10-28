144d819a44ade8dca087a6b36d420d0e72db9e75
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2016-10-28 18:02:35 -0400

    Add an operator properties panel
    It is a little lame right now as it uses the label (there is no exposed
    description for the operator). This can be extended, and I think it would
    ideally show the GUI for the operator once we are able to show that after
    initial creation.

---
+++ tomviz/OperatorPropertiesPanel.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+  This source file is part of the tomviz project.
+
+  Copyright Kitware, Inc.
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+******************************************************************************/
+#ifndef tomvizOperatorPropertiesPanel_h
+#define tomvizOperatorPropertiesPanel_h
+
+#include <QWidget>
+
+#include <QPointer>
+
+class QLabel;
+
+namespace tomviz {
+class Operator;
+
+class OperatorPropertiesPanel : public QWidget
+{
+  Q_OBJECT
+
+public:
+  OperatorPropertiesPanel(QWidget* parent = nullptr);
+  virtual ~OperatorPropertiesPanel();
+
+private slots:
+  void setOperator(Operator*);
+  void updatePanel();
+
+private:
+  Q_DISABLE_COPY(OperatorPropertiesPanel)
+
+  QPointer<Operator> m_activeOperator = nullptr;
+  QLabel* m_description = nullptr;
+};
+}
+
+#endif
