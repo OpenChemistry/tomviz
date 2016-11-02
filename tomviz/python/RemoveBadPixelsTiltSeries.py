99e24613adf254a73a0642a9aa3f2fcd08d36967
Yi Jiang <yj245@cornell.edu>
2016-11-02 11:13:54 -0400

    added python function

--- tomviz/python/RemoveBadPixelsTiltSeries.py
+++ tomviz/python/RemoveBadPixelsTiltSeries.py
@@ -2,23 +2,31 @@ def transform_scalars(dataset):
     """Remove bad pixels in tilt series."""
 
     #----USER SPECIFIED VARIABLES-----#
-    ###s###
+    ###threshold###
     #---------------------------------#
 
     from tomviz import utils
     import scipy.ndimage
     import numpy as np
-    
-    tiltSeries = utils.get_array(dataset)
-    
+
+    tiltSeries = utils.get_array(dataset).astype(np.float32)
+
     for i in range(tiltSeries.shape[2]):
-        tiltImage = tiltSeries[:, :, i]
-        medianFilteredImage = scipy.ndimage.filters.median_filter(tiltImage, 2)
-        differenceImage = tiltImage - medianFilteredImage
-        sigma = np.std(differenceImage.flatten())
-        badPixelsMask = abs(tiltSeries[:, :, i]) > s * sigma
-        tiltImage[badPixelsMask] = medianFilteredImage[badPixelsMask]
-        tiltSeries[:, :, i] = tiltImage
+        I = tiltSeries[:, :, i]
+        I_pad = np.lib.pad(I, (1, 1), 'edge')
+
+        # calculate standard deviation in a 3 x 3 window
+        averageI2 = scipy.ndimage.filters.uniform_filter(I_pad ** 2)
+        averageI = scipy.ndimage.filters.uniform_filter(I_pad)
+        std = np.sqrt(abs(averageI2 - averageI**2))[1:-1, 1:-1]
+
+        medianI = scipy.ndimage.filters.median_filter(I_pad, 2)[1:-1, 1:-1]
+
+        #identiy bad pixels
+        badPixelsMask = abs(I - medianI) > std * threshold
+
+        I[badPixelsMask] = medianI[badPixelsMask]
+        tiltSeries[:, :, i] = I
 
     # Set the result as the new scalars.
     utils.set_array(dataset, tiltSeries)
