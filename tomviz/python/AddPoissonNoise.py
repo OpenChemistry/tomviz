2dc291a9416d0e031f3e1f06a129870b9e3b7d50
Yi Jiang <yj245@cornell.edu>
2016-11-15 10:28:26 -0500

    added poisson noise for tomography simulation

---
+++ tomviz/python/AddPoissonNoise.py
@@ -0,0 +1,41 @@
+import numpy as np
+from tomviz import utils
+import tomviz.operators
+
+
+class AddPoissonNoiseOperator(tomviz.operators.CancelableOperator):
+
+    def transform_scalars(self, dataset):
+        """Add Poisson noise to tilt images"""
+        self.progress.maximum = 1
+
+        #----USER SPECIFIED VARIABLES-----#
+        ###N###
+        #---------------------------------#
+
+        tiltSeries = utils.get_array(dataset).astype(float)
+        if tiltSeries is None:
+            raise RuntimeError("No scalars found!")
+
+        Ndata = tiltSeries.shape[0] * tiltSeries.shape[1]
+
+        self.progress.maximum = tiltSeries.shape[2]
+        step = 0
+        for i in range(tiltSeries.shape[2]):
+            if self.canceled:
+                return
+
+            tiltImage = tiltSeries[:, :, i].copy()
+            tiltImage = tiltImage / np.sum(tiltSeries[:, :, i]) * (Ndata * N)
+            tiltImage = np.random.poisson(tiltImage)
+            tiltImage = tiltImage * np.sum(tiltSeries[:, :, i]) / (Ndata * N)
+
+            #calculate signal-to-noise ratio
+            snr = np.mean(tiltSeries[:, :, i]) / \
+                np.std(tiltSeries[:, :, i] - tiltImage)
+            print snr
+            tiltSeries[:, :, i] = tiltImage.copy()
+            step += 1
+            self.progress.update(step)
+
+        utils.set_array(dataset, tiltSeries)
