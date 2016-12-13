ae6c5ad0f2b558dac075e7297237def40343418d
Cory Quammen <cory.quammen@kitware.com>
2016-12-13 14:47:04 -0500

    Use utility function to compute shape label object attributes

--- tomviz/python/LabelObjectAttributes.py
+++ tomviz/python/LabelObjectAttributes.py
@@ -18,29 +18,11 @@ def transform_scalars(dataset):
         raise Exception(
             "Label Object Attributes works only on images with integral types.")
 
-    try:
-        # Get an ITK image from the data set
-        itk_image = itkutils.convert_vtk_to_itk_image(dataset)
-        itk_image_type = type(itk_image)
-
-        # Get an appropriate LabelImageToShapelLabelMapFilter type for the
-        # input.
-        inputTypes = [x[0] for x in itk.LabelImageToShapeLabelMapFilter.keys()]
-        filterTypeIndex = inputTypes.index(itk_image_type)
-        if filterTypeIndex < 0:
-            raise Exception("No suitable filter type for input type %s" %
-                            type(itk_image_type))
-
-        # Now take the connected components results and compute things like
-        # volume and surface area.
-        shape_filter = \
-            itk.LabelImageToShapeLabelMapFilter.values()[filterTypeIndex].New()
-        shape_filter.SetInput(itk_image)
-        shape_filter.Update()
-
+    try:        
         # Set up arrays to hold the shape attribute data
-        label_map = shape_filter.GetOutput()
-        num_label_objects = label_map.GetNumberOfLabelObjects()
+        shape_label_map = itkutils.get_label_object_attributes(dataset)
+
+        num_label_objects = shape_label_map.GetNumberOfLabelObjects()
 
         column_names = ['SurfaceArea', 'Volume', 'SurfaceAreaToVolumeRatio']
         import numpy as np
@@ -48,7 +30,7 @@ def transform_scalars(dataset):
         table = np.zeros((num_label_objects, len(column_names)))
 
         for i in xrange(0, num_label_objects):
-            label_object = label_map.GetNthLabelObject(i)
+            label_object = shape_label_map.GetNthLabelObject(i)
             surface_area = label_object.GetPerimeter()
             table[i, 0] = surface_area
             volume = label_object.GetPhysicalSize()
