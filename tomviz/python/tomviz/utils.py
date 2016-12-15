c37298c098cb2a8275d02508088a3fa13a316e62
Cory Quammen <cory.quammen@kitware.com>
2016-12-15 11:55:30 -0500

    Add connected_components function
    This function is useful in a variety of tasks, so make it a utility
    function.

--- tomviz/python/tomviz/utils.py
+++ tomviz/python/tomviz/utils.py
@@ -135,6 +135,73 @@ def get_coordinate_arrays(dataset):
     return (xx, yy, zz)
 
 
+def connected_components(dataset, background_value=0):
+    try:
+        import itk
+        import itkTypes
+        import vtk
+        from tomviz import itkutils
+    except Exception as exc:
+        print("Could not import necessary module(s)")
+        print(exc)
+
+    scalarType = dataset.GetScalarType()
+    if scalarType == vtk.VTK_FLOAT or scalarType == vtk.VTK_DOUBLE:
+        raise Exception(
+            "Connected Components works only on images with integral types.")
+
+    # Add a try/except around the ITK portion. ITK exceptions are
+    # passed up to the Python layer, so we can at least report what
+    # went wrong with the script, e.g,, unsupported image type.
+    try:
+        # Get the ITK image. The input is assumed to have an integral type.
+        # Take care of casting to an unsigned short image so we can store up
+        # to 65,535 connected components (the number of connected components
+        # is limited to the maximum representable number in the voxel type
+        # of the input image in the ConnectedComponentsFilter).
+        itk_image = itkutils.convert_vtk_to_itk_image(dataset, itkTypes.US)
+        itk_image_type = type(itk_image)
+
+        # ConnectedComponentImageFilter
+        connected_filter = itk.ConnectedComponentImageFilter[
+            itk_image_type, itk_image_type].New()
+        connected_filter.SetBackgroundValue(background_value)
+        connected_filter.SetInput(itk_image)
+
+        # Relabel filter. This will compress the label numbers to a
+        # continugous range between 1 and n where n is the number of
+        # labels. It will also sort the components from largest to
+        # smallest, where the largest component has label 1, the
+        # second largest has label 2, and so on...
+        relabel_filter = itk.RelabelComponentImageFilter[
+            itk_image_type, itk_image_type].New()
+        relabel_filter.SetInput(connected_filter.GetOutput())
+        relabel_filter.SortByObjectSizeOn()
+        relabel_filter.Update()
+
+        itk_image_data = relabel_filter.GetOutput()
+        label_buffer = itk.PyBuffer[
+            itk_image_type].GetArrayFromImage(itk_image_data)
+
+        # Flip the labels so that the largest component has the highest label
+        # value, e.g., the labeling ordering by size goes from [1, 2, ... N] to
+        # [N, N-1, N-2, ..., 1]. Note that zero is the background value, so we
+        # do not want to change it.
+        import numpy as np
+        minimum = 1 # Minimum label is always 1, background is 0
+        maximum = np.max(label_buffer)
+
+        # Try more memory-efficient approach
+        gt_zero = label_buffer > 0
+        label_buffer[gt_zero] = minimum - label_buffer[gt_zero] + maximum
+
+        set_array(dataset, label_buffer)
+    except Exception as exc:
+        print("Exception encountered while running ConnectedComponents")
+        print(exc)
+        raise exc
+
+
 def label_object_principal_axes(dataset, label_value):
     import numpy as np
     from tomviz import utils
