6bc68fa898083a696c931ca4fff82984eeec3131
Chris Harris <chris.harris@kitware.com>
2017-02-02 16:44:28 -0500

    Add doc strings to AbstractSource

--- acquisition/tomviz/acquisition/__init__.py
+++ acquisition/tomviz/acquisition/__init__.py
@@ -2,16 +2,33 @@ from abc import abstractmethod, ABCMeta
 
 
 class AbstractSource(object):
+    """
+    Abstract interface implemented to define an acquistion source.
+    """
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def set_tilt_angle(self, angle):
+        """
+        Set the tilt angle.
+        :param angle: The title angle to set.
+        :type angle: int
+        :returns: The set tilt angle
+        """
         pass
 
     @abstractmethod
     def preview_scan(self):
+        """
+        Peforms a preview scan.
+        :returns: The 2D tiff generate by the scan
+        """
         pass
 
     @abstractmethod
     def stem_acquire(self):
+        """
+        Peforms STEM acquire
+        :returns: The 2D tiff generate by the scan
+        """
         pass
