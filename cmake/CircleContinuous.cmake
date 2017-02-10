ac1d3e7fcfe021c95288b3ec9b5a06c6a46a95a8
Chris Harris <chris.harris@kitware.com>
2017-02-10 10:40:14 -0500

    Upload CircleCI results to CDash

---
+++ cmake/CircleContinuous.cmake
@@ -0,0 +1,29 @@
+set(CTEST_SOURCE_DIRECTORY "/tomviz")
+set(CTEST_BINARY_DIRECTORY "/tomviz-build")
+# We need to set this otherwise we yet 255 as our return code!
+set(CTEST_COMMAND ctest)
+include(${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake)
+set(CTEST_SITE "CircleCI")
+set(CTEST_BUILD_NAME "Linux-$ENV{CIRCLE_BRANCH}")
+set(CTEST_CMAKE_GENERATOR "Ninja")
+set(cfg_options
+  -DCMAKE_BUILD_TYPE:STRING=Release
+  -DENABLE_TESTING:BOOL=ON
+  -DParaView_DIR:PATH=$ENV{PARAVIEW_DIR}
+  -DITK_DIR:PATH=$ENV{ITK_DIR}
+  -DQt5_DIR:PATH=$ENV{QT5_DIR}
+  -DGTEST_LIBRARY:PATH=$ENV{GTEST_LIBRARY}
+  -DGTEST_MAIN_LIBRARY:PATH=$ENV{GTEST_MAIN_LIBRARY}
+  -DGTEST_INCLUDE_DIR:PATH=$ENV{GTEST_INCLUDE_DIR})
+
+ctest_start("Continuous")
+ctest_configure(OPTIONS "${cfg_options}")
+ctest_build()
+ctest_test(RETURN_VALUE rv)
+ctest_submit()
+
+file(REMOVE "${CTEST_BINARY_DIRECTORY}/failure")
+if(NOT rv EQUAL 0)
+  file(WRITE "${CTEST_BINARY_DIRECTORY}/failure" "${rv}")
+  message(FATAL_ERROR "Test failures occurred.")
+endif()
