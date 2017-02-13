9c0f7b1724d7ebd99abd1251bcc831d8e428a2c5
Chris Harris <chris.harris@kitware.com>
2017-02-13 11:30:48 -0500

    GradientMagnitude_Sobel: Preserve array order

--- tomviz/python/GradientMagnitude_Sobel.py
+++ tomviz/python/GradientMagnitude_Sobel.py
@@ -9,8 +9,9 @@ def transform_scalars(dataset):
     array = array.astype(np.float32)
 
     # Transform the dataset
-    result = scipy.ndimage.filters.generic_gradient_magnitude(
-        array, scipy.ndimage.filters.sobel)
+    result = np.empty_like(array)
+    scipy.ndimage.filters.generic_gradient_magnitude(
+        array, scipy.ndimage.filters.sobel, output=result)
 
     # Set the result as the new scalars.
     utils.set_array(dataset, result)
