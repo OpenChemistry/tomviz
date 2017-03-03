17570f90c4e2241d8ac2e84d8df7cfa354451e1e
Chris Harris <chris.harris@kitware.com>
2017-03-03 14:05:53 -0500

    Remove script to convert PR number into branch name
    This only works intermittently due to rate limiting

--- scripts/common/pr_number_to_ref.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-import requests
-
-url = 'https://api.github.com/repos/OpenChemistry/tomviz/pulls/%s' % sys.argv[1]
-request = requests.get(url)
-request.raise_for_status()
-print(request.json()['head']['ref'])
