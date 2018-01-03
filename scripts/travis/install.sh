73016a210552ac015a75c0122c09b0b11389258a
Chris Harris <chris.harris@kitware.com>
2018-01-03 14:13:03 -0500

    Work around Mac OSX pip issue

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # For acquisition testing
     sudo -H python -m ensurepip
     sudo -H pip install -U pip
-    sudo -H pip install -r $TRAVIS_BUILD_DIR/acquisition/requirements-dev.txt
+    sudo -H pip install --ignore-installed -r $TRAVIS_BUILD_DIR/acquisition/requirements-dev.txt
     $TRAVIS_BUILD_DIR/scripts/travis/download_deps.py
 else
     pip install flake8
