39bb919f1ed16f1077fb8745457b4c719d673abf
Chris Harris <chris.harris@kitware.com>
2018-01-29 15:26:56 -0500

    WIP: Debug

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -5,6 +5,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     sudo -H python -m ensurepip
     sudo -H pip install -U pip
     sudo -H pip install --ignore-installed -r $TRAVIS_BUILD_DIR/acquisition/requirements-dev.txt
+    sudo chmod u+w ./python/lib/libpython3.6m.dylib
+    ls -la ./python/lib/libpython3.6m.dylib
     $TRAVIS_BUILD_DIR/scripts/travis/download_deps.py
 else
     pip install flake8
