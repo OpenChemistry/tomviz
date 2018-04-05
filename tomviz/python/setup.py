f50ae385fff9f87b5d57edd1b6a11fad32ece5ad
Chris Harris <chris.harris@kitware.com>
2018-04-05 10:40:51 -0400

    Add missing setup.py

---
+++ tomviz/python/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tomviz-external',
+    version='0.0.1',
+    description='Tomviz python external execution infrastructure.',
+    author='Kitware, Inc.',
+    author_email='kitware@kitware.com',
+    url='https://www.tomviz.org/',
+    license='BSD 3-Clause',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD 3-Clause',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ],
+    packages=find_packages(),
+    install_requires=['tqdm', 'h5py', 'numpy', 'click'],
+    entry_points={
+        'console_scripts': [
+            'tomviz = tomviz.__main__:main'
+        ]
+    }
+)
