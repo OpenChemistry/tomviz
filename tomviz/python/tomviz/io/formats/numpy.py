5d6e33313c3f224d70383082f82d1c053c910a7f
Alessandro Genova <alessandro.genova@kitware.com>
2018-05-29 10:07:31 -0400

    Tweaks to the IO/Reader/Writer dependency

---
+++ tomviz/python/tomviz/io/formats/numpy.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+#  This source file is part of the tomviz project.
+#
+#  Copyright Kitware, Inc.
+#
+#  This source code is released under the New BSD License, (the "License").
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+###############################################################################
+
+import numpy as np
+
+from tomviz.io.base import Writer
+from tomviz.io.base import Reader
+
+import tomviz.utils
+from tomviz.io.base import FileType, IOBase
+
+from vtk import vtkImageData
+
+
+class NumpyBase(IOBase):
+
+    def __init__(self, mode):
+        super(NumpyBase, self).__init__('binary')
+
+    @staticmethod
+    def file_type():
+        return FileType('NumPy binary format', ['npy'])
+
+
+class NumpyWriter(Writer, NumpyBase):
+    def __init__(self):
+        super(NumpyWriter, self).__init__()
+
+    def write(self, path, data_object):
+        data = tomviz.utils.get_array(data_object)
+        with self.open(path, write=True) as f:
+            np.save(f, data)
+
+
+class NumpyReader(Reader, NumpyBase):
+    def __init__(self):
+        super(NumpyReader, self).__init__()
+
+    def read(self, path):
+        with self.open(path) as f:
+            data = np.load(f)
+
+        image_data = vtkImageData()
+        (x, y, z) = data.shape
+
+        image_data.SetOrigin(0, 0, 0)
+        image_data.SetSpacing(1, 1, 1)
+        image_data.SetExtent(0, x - 1, 0, y - 1, 0, z - 1)
+        tomviz.utils.set_array(image_data, data)
+
+        return image_data
