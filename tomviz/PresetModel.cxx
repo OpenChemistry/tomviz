2dbd0147d5344e1fa99e1f0f459139b97b7d7176
Brianna Major <brianna.major@kitware.com>
2019-06-25 16:48:33 -0400

    Find presets relative to app directory; made suggested fixes
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- tomviz/PresetModel.cxx
+++ tomviz/PresetModel.cxx
@@ -11,12 +11,11 @@
 
 namespace tomviz {
 
-// ctor
 PresetModel::PresetModel(QObject* parent) : QAbstractTableModel(parent)
 {
   vtkNew<vtkSMTransferFunctionPresets> Presets;
   auto begin = Presets->GetNumberOfPresets();
-  QString file = "../tomviz/tomviz/resources/matplotlib_cmaps.json";
+  QString file = getMatplotlibColorMapFile();
   Presets->ImportPresets(file.toStdString().c_str());
 
   pqPresetToPixmap PixMapRenderer;
@@ -27,19 +26,16 @@ PresetModel::PresetModel(QObject* parent) : QAbstractTableModel(parent)
   }
 }
 
-// the number of rows the view should display
 int PresetModel::rowCount(const QModelIndex& id) const
 {
   return id.isValid() ? 0 : m_Pixmaps.size();
 }
 
-// the number of columns the view should display
 int PresetModel::columnCount(const QModelIndex& /*parent*/) const
 {
   return 1;
 }
 
-// returns what value should be in the cell at a given index
 QVariant PresetModel::data(const QModelIndex& index, int role) const
 {
   switch (role) {
@@ -56,22 +52,18 @@ QVariant PresetModel::data(const QModelIndex& index, int role) const
   return QVariant();
 }
 
-QVariant PresetModel::headerData(int, Qt::Orientation orientation,
-                                 int role) const
+QVariant PresetModel::headerData(int, Qt::Orientation /*orientation*/,
+				 int /*role*/) const
 {
-  if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
-    return QString("Presets");
-  }
-
   return QVariant();
 }
-
+  
 void PresetModel::setName(const QModelIndex& index)
 {
   m_name = m_Pixmaps[index.row()].first;
 }
 
-QString PresetModel::getName()
+QString PresetModel::presetName()
 {
   return m_name;
 }
@@ -80,5 +72,27 @@ void PresetModel::changePreset(const QModelIndex& index)
 {
   emit setName(index);
   emit applyPreset();
+}
+
+QString PresetModel::getMatplotlibColorMapFile()
+{
+  QString path = QApplication::applicationDirPath() +
+                 "/../share/tomviz/matplotlib_cmaps.json";
+  QFile file(path);
+  if (file.exists()) {
+    return path;
+  }
+// On OSX the above doesn't work in a build tree.  It is fine
+// for superbuilds, but the following is needed in the build tree
+// since the executable is three levels down in bin/tomviz.app/Contents/MacOS/
+#ifdef __APPLE__
+  else {
+    path = QApplication::applicationDirPath() +
+           "/../../../../share/tomviz/matplotlib_cmaps.json";
+    return path;
+  }
+#else
+  return "";
+#endif
 }
 } // namespace tomviz
