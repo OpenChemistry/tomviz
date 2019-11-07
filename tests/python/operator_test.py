bd351bf6f512c27af7e5417b861569f7fd864c20
Patrick Avery <patrick.avery@kitware.com>
2019-11-07 11:06:18 -0500

    Use new transform() API for operator_test
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tests/python/operator_test.py
+++ tests/python/operator_test.py
@@ -18,8 +18,8 @@ import tomviz
 tomviz._wrapping = mock.MagicMock()
 sys.modules['tomviz._wrapping'] = mock.MagicMock()
 from tomviz._internal import find_operator_class # noqa
-from tomviz._internal import find_transform_scalars_function # noqa
-from tomviz._internal import find_transform_scalars, is_cancelable # noqa
+from tomviz._internal import find_transform_from_module # noqa
+from tomviz._internal import find_transform_function, is_cancelable # noqa
 from tomviz._internal import find_operators # noqa
 
 
@@ -36,29 +36,29 @@ def test_find_operator_class():
         find_operator_class(two)
 
 
-def test_find_transform_scalars_function():
+def test_find_transform_from_module():
     # No function
-    assert find_transform_scalars_function(simple) is None
+    assert find_transform_from_module(simple) is None
 
     # Module with a function
-    found = find_transform_scalars_function(function)
+    found = find_transform_from_module(function)
     assert (found == function.transform_scalars)
 
 
-def test_find_transform_scalars():
+def test_find_transform_function():
     # Module with a function
-    found = find_transform_scalars(function, None)
+    found = find_transform_function(function, None)
     assert (found == function.transform_scalars)
 
     # Module with operator class
-    func = find_transform_scalars(simple, None)
+    func = find_transform_function(simple, None)
     assert isinstance(func.__self__, simple.SimpleOperator)
     assert func.__func__.__name__ == 'transform_scalars'
     assert func(None)
 
     # Module with none
     with pytest.raises(Exception):
-        find_transform_scalars(unittest, None)
+        find_transform_function(unittest, None)
 
 
 def test_is_cancelable():
