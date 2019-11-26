d1cb82aadad4b7640d71a61630763b8e4dd79748
Patrick Avery <patrick.avery@kitware.com>
2019-11-26 15:41:46 -0500

    Use python 3.8 for all azure-pipelines runs
    Windows does not appear to have a library for python 3.7 available,
    requiring us to move to python 3.8. We can move all of the other
    operating systems forward as well, as long as we install python 3.8
    h5py wheels from scipy (they are not available yet on PyPI), and we
    skip installing pyfftw, as it does not have wheels either.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/azure-pipelines/install_python_deps.sh
+++ scripts/azure-pipelines/install_python_deps.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+if [[ $AGENT_OS == 'Linux' ]]; then
+  # h5py wheels for Linux python3.8 aren't on PyPI yet. Install from scipy.
+  # Remove after this is fixed: https://github.com/h5py/h5py/issues/1410
+  pip3 install http://wheels.scipy.org/h5py-2.10.0-cp38-cp38-manylinux1_x86_64.whl
+elif [[ $AGENT_OS == 'Darwin' ]]; then
+  # h5py wheels for Mac python3.8 aren't on PyPI yet. Install from scipy.
+  # Remove after this is fixed: https://github.com/h5py/h5py/issues/1410
+  pip3 install http://wheels.scipy.org/h5py-2.10.0-cp38-cp38-macosx_10_9_x86_64.whl
+fi
+
 pip3 install --upgrade pip setuptools wheel
 pip3 install numpy scipy
 pip3 install -r acquisition/requirements-dev.txt
 pip3 install -r tests/python/requirements-dev.txt
-pip3 install tomviz/python
 pip3 install acquisition
+
+# We don't need pyfftw for the tests, it doesn't have wheels yet for
+# python3.8, and it can be a pain to build, so skip installing it.
+# Install the other dependencies manually.
+pip3 install --no-deps tomviz/python
+pip3 install tqdm h5py numpy==1.16.4 click scipy itk
