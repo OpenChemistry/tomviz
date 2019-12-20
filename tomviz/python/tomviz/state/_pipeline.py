0bff34400d912806a9d831f5e0436082d359a531
Chris Harris <chris.harris@kitware.com>
2019-12-20 15:29:40 -0500

    Fix singleton to work with wrapped manager class
    Signed-off-by: Chris Harris <chris.harris@kitware.com>

--- tomviz/python/tomviz/state/_pipeline.py
+++ tomviz/python/tomviz/state/_pipeline.py
@@ -3,13 +3,8 @@ from tomviz._wrapping import PipelineStateManagerBase
 class PipelineStateManager(PipelineStateManagerBase):
     _instance = None
 
-    # Need to define a constructor as the implementation on the C++ side is
-    # static.
-    def __init__(self):
-        pass
-
-    def __call__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(PipelineStateManager, cls).__call__()
+            cls._instance = PipelineStateManagerBase.__new__(cls, *args, **kwargs)
 
-        return cls._instances
+        return cls._instance
