a4982987355374424cf859f654831e9070334a35
Chris Harris <chris.harris@kitware.com>
2021-01-28 09:26:11 -0500

    Close dataset after getting variable

--- tomviz/python/tomviz/io/_databroker.py
+++ tomviz/python/tomviz/io/_databroker.py
@@ -76,20 +76,24 @@ def variables(catalog_name, run_uid, table):
 
 
 def load_variable(catalog_name, run_uid, table, variable):
-    data = catalog[catalog_name][run_uid][table].read()[variable].data
-    shape = data.shape
-    data = data.reshape((shape[0]*shape[1], shape[2], shape[3]))
+    dataset = catalog[catalog_name][run_uid][table].read()
+    try:
+        data = dataset[variable].data
+        shape = data.shape
+        data = data.reshape((shape[0]*shape[1], shape[2], shape[3]))
 
-    # Convert to Fortran ordering
-    data = np.asfortranarray(data)
+        # Convert to Fortran ordering
+        data = np.asfortranarray(data)
 
-    image_data = vtkImageData()
-    (x, y, z) = data.shape
+        image_data = vtkImageData()
+        (x, y, z) = data.shape
 
-    # Metadata?
-    image_data.SetOrigin(0, 0, 0)
-    image_data.SetSpacing(1, 1, 1)
-    image_data.SetExtent(0, x - 1, 0, y - 1, 0, z - 1)
-    tomviz.utils.set_array(image_data, data)
+        # Metadata?
+        image_data.SetOrigin(0, 0, 0)
+        image_data.SetSpacing(1, 1, 1)
+        image_data.SetExtent(0, x - 1, 0, y - 1, 0, z - 1)
+        tomviz.utils.set_array(image_data, data)
+    finally:
+        dataset.close()
 
     return image_data
