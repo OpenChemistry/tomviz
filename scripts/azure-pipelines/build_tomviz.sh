f6c78f9441fc424303324b4ae6f9141aef2d2112
Patrick Avery <patrick.avery@kitware.com>
2021-02-03 20:14:31 -0600

    Try with $(dirname $(which python))
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/azure-pipelines/build_tomviz.sh
+++ scripts/azure-pipelines/build_tomviz.sh
@@ -4,7 +4,7 @@ if [[ $AGENT_OS == 'Darwin' ]]; then
   # CMake can't find Qt on mac os unless we do this
   CMAKE_EXTRA_ARGS="-DCMAKE_PREFIX_PATH=/usr/local/opt/qt/ "
   # Resort to brute force to get CMake to find the right library!
-  CMAKE_EXTRA_ARGS+="-DPython3_LIBRARY=/Users/runner/hostedtoolcache/Python/3.8.7/x64/lib/libpython3.8.dylib "
+  CMAKE_EXTRA_ARGS+="-DPython3_LIBRARY=$(dirname $(which python))/../lib/libpython3.8.dylib "
 elif [[ $AGENT_OS == 'Windows_NT' ]]; then
   export CC=cl
   export CXX=cl
