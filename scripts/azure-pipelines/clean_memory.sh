f4171613b02a657cce613eabaa251f813928b279
Patrick Avery <patrick.avery@kitware.com>
2021-04-22 12:16:18 -0500

    Perform memory cleaning for mac as well
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- scripts/azure-pipelines/clean_memory.sh
+++ scripts/azure-pipelines/clean_memory.sh
@@ -2,7 +2,7 @@
 
 # Clean up some memory so we can tar and upload the paraview cache
 
-if [[ $AGENT_OS == 'Linux' ]]; then
+if [[ $AGENT_OS != 'Windows_NT' ]]; then
   rm -rf $PARAVIEW_SOURCE_FOLDER
   rm -rf $PARAVIEW_BUILD_FOLDER
   rm -rf $BUILD_SOURCESDIRECTORY
