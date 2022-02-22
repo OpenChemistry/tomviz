7cf948d25e5723ace301a61dffa4bf9460a67fc2
Patrick Avery <patrick.avery@kitware.com>
2022-02-21 18:08:13 -0600

    Add dependencies to data source loading
    This allows us to load state files where one operator selected a
    another data source. The other data source must be loaded first
    before that operator can be ran.
    
    This finds a data source's dependencies, loads in the dependencies
    first, waits for their pipeline to finish if the dependency is
    a child data source, and then loads in the data source.
    
    It seems to be working well in the examples I've tried.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tomviz/modules/ModuleManager.cxx
+++ tomviz/modules/ModuleManager.cxx
@@ -47,6 +47,7 @@
 
 #include <QDebug>
 #include <QDir>
+#include <QEventLoop>
 #include <QJsonArray>
 #include <QMap>
 #include <QMessageBox>
@@ -57,6 +58,15 @@
 #include <sstream>
 
 namespace tomviz {
+
+static QList<QJsonObject> toObjectList(const QJsonArray& array)
+{
+  QList<QJsonObject> ret;
+  for (const auto& item : array) {
+    ret.append(item.toObject());
+  }
+  return ret;
+}
 
 class ModuleManager::MMInternals
 {
@@ -154,6 +164,71 @@ public:
       }
       dataSourceState["reader"] = reader;
     }
+  }
+
+  QStringList dataSourceDependencies(const QJsonObject& ds)
+  {
+    QStringList deps;
+    if (!ds.contains("operators")) {
+      return deps;
+    }
+
+    for (auto opVal : ds["operators"].toArray()) {
+      auto op = opVal.toObject();
+      auto args = op.value("arguments").toObject();
+      for (auto& key : args.keys()) {
+        auto value = args[key];
+        if (value.isString() && value.toString().startsWith("0x")) {
+          deps.append(value.toString());
+        }
+      }
+
+      if (op.contains("dataSources")) {
+        for (auto output : op["dataSources"].toArray()) {
+          deps.append(dataSourceDependencies(output.toObject()));
+        }
+      }
+    }
+
+    return deps;
+  }
+
+  QJsonObject rootDataSourceDependency(QString dep,
+                                       QList<QJsonObject>& dataSources)
+  {
+    for (auto& ds : dataSources) {
+      if (ds.value("id").toString() == dep) {
+        return ds;
+      }
+
+      if (!ds.contains("operators")) {
+        continue;
+      }
+
+      for (auto opVal : ds["operators"].toArray()) {
+        auto op = opVal.toObject();
+        if (!op.contains("dataSources")) {
+          continue;
+        }
+
+        auto childDataSources = toObjectList(op["dataSources"].toArray());
+
+        // See if the child has it
+        auto result = rootDataSourceDependency(dep, childDataSources);
+        if (!result.isEmpty()) {
+          return ds;
+        }
+      }
+    }
+
+    // Didn't find it...
+    return {};
+  }
+
+  bool isChildDataSourceDependency(QString dep, QList<QJsonObject>& dataSources)
+  {
+    auto root = rootDataSourceDependency(dep, dataSources);
+    return root.value("id").toString() != dep;
   }
 };
 
@@ -1034,10 +1109,7 @@ void ModuleManager::onPVStateLoaded(vtkPVXMLElement*,
   // Load up all of the data sources.
   if (m_loadDataSources && m_stateObject["dataSources"].isArray()) {
     auto dataSources = m_stateObject["dataSources"].toArray();
-    foreach (auto ds, dataSources) {
-      auto dsObject = ds.toObject();
-      loadDataSource(dsObject);
-    }
+    loadDataSources(dataSources);
   }
 
   // Load up all of the molecule sources.
@@ -1167,6 +1239,62 @@ bool ModuleManager::hasDataSources()
 bool ModuleManager::hasMoleculeSources()
 {
   return !d->MoleculeSources.empty();
+}
+
+void ModuleManager::loadDataSources(const QJsonArray& dataSourcesArray)
+{
+  // Data source arguments in operators make it so that we have to
+  // load in the data sources in a proper order, or else the arguments
+  // will not be able to be resolved for the operator to use.
+
+  auto dataSources = toObjectList(dataSourcesArray);
+  QMap<QString, DataSource*> alreadyLoaded;
+
+  // This local load data source function ensures we load dependencies
+  // before we load in the requested data source. It also ensures that
+  // we do not load in any data sources twice.
+  std::function<void(QJsonObject ds)> localLoadDataSource;
+  localLoadDataSource = [&dataSources, &alreadyLoaded, &localLoadDataSource,
+                         this](QJsonObject ds) {
+    auto id = ds.value("id").toString();
+    if (alreadyLoaded.contains(id)) {
+      // Already have this one
+      return;
+    }
+
+    // First, see if it has any dependencies.
+    auto deps = d->dataSourceDependencies(ds);
+    if (deps.empty()) {
+      // No dependencies. Go ahead and load it.
+      alreadyLoaded[id] = loadDataSource(ds);
+      return;
+    }
+
+    // Load the dependencies first.
+    for (auto& dep : deps) {
+      auto rootObj = d->rootDataSourceDependency(dep, dataSources);
+      localLoadDataSource(rootObj);
+
+      // If it is a child dependency, wait for the pipeline to finish
+      if (d->isChildDataSourceDependency(dep, dataSources)) {
+        auto rootId = rootObj.value("id").toString();
+        auto* depDataSource = alreadyLoaded[rootId];
+        if (depDataSource->pipeline()->isRunning()) {
+          QEventLoop loop;
+          connect(depDataSource->pipeline(), &Pipeline::finished, &loop,
+                  &QEventLoop::quit);
+          loop.exec();
+        };
+      }
+    }
+
+    // Now that the dependencies are loaded, we can load the data source...
+    alreadyLoaded[id] = loadDataSource(ds);
+  };
+
+  for (auto& ds : dataSources) {
+    localLoadDataSource(ds);
+  }
 }
 
 DataSource* ModuleManager::loadDataSource(QJsonObject& dsObject)
