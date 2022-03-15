0fdf4585dbca8d07ea6e27752379371ee1d8b02e
Patrick Avery <patrick.avery@kitware.com>
2022-03-15 14:29:11 -0500

    Fix clang-format
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tomviz/pybind11/Wrapping.cxx
+++ tomviz/pybind11/Wrapping.cxx
@@ -42,18 +42,17 @@ PYBIND11_PLUGIN(_wrapping)
                            "Get the dark image data")
     .def_property_readonly("white_data", &DataSourceBase::whiteData,
                            "Get the white image data")
-    .def_property_readonly(
-      "file_name", [](const DataSourceBase& b) { return b.fileName(); },
-      "Get the file from which the data source was loaded")
-    .def_property_readonly(
-      "metadata",
-      [](const DataSourceBase& b) {
-        auto* obj = tomviz::toPyObject(b.metadata());
-        // The PyObject* reference is currently unmanaged.
-        // Therefore, we steal it, rather than borrow.
-        return py::reinterpret_steal<py::dict>(obj);
-      },
-      "Get the data source metadata");
+    .def_property_readonly("file_name",
+                           [](const DataSourceBase& b) { return b.fileName(); },
+                           "Get the file from which the data source was loaded")
+    .def_property_readonly("metadata",
+                           [](const DataSourceBase& b) {
+                             auto* obj = tomviz::toPyObject(b.metadata());
+                             // The PyObject* reference is currently unmanaged.
+                             // Therefore, we steal it, rather than borrow.
+                             return py::reinterpret_steal<py::dict>(obj);
+                           },
+                           "Get the data source metadata");
 
   py::class_<PipelineStateManager>(m, "PipelineStateManagerBase")
     .def(py::init())
