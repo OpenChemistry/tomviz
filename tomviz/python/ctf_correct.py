dc79aeab6793287a72a5916c0cb48e8adefe770e
jtschwar <jtschwar@gmail.com>
2022-04-14 15:04:31 -0400

    Add Ctf correction operator
    Signed-off-by: jtschwar <jtschwar@gmail.com>

--- tomviz/python/ctf_correct.py
+++ tomviz/python/ctf_correct.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-# Given an dataset containing one or more 2D images, 
+# Given an dataset containing one or more 2D images,
 # apply CTF operations on them.
 def transform(dataset, apix=None, df1=None, df2=None, ast=None, ampcon=None,
               cs=None, kev=None, ctf_method=None, snr=None):
@@ -12,7 +12,7 @@ def transform(dataset, apix=None, df1=None, df2=None, ast=None, ampcon=None,
 
     # Main loop
     for i in range(tiltSeries.shape[2]):
-        tiltSeries[:,:,i] = correct_CTF(tiltSeries[:,:,i], df1, df2, ast,
+        tiltSeries[:, :, i] = correct_CTF(tiltSeries[:,:,i], df1, df2, ast,
                                         ampcon, cs, kev, apix,
                                         methods[ctf_method], snr)
 
@@ -23,7 +23,7 @@ def transform(dataset, apix=None, df1=None, df2=None, ast=None, ampcon=None,
 def correct_CTF(img, DF1, DF2, AST, AmpCon, Cs, kV, apix, method, snr):
 
     CTFim = -CTF(img.shape, DF1, DF2, AST, AmpCon, Cs, kV, apix)
-		
+
     FTim = np.fft.rfftn(img)
     if method == 'wiener_filter':
         CTFcor = np.fft.irfftn((FTim * CTFim) / (CTFim * CTFim + snr))
@@ -36,13 +36,13 @@ def correct_CTF(img, DF1, DF2, AST, AmpCon, Cs, kV, apix, method, snr):
     return CTFcor
 
 
-# Generates 2D CTF Function 
+# Generates 2D CTF Function
 # Underfocus is positive following conventions of FREALIGN and most packages
 # DF - (in Microns)
 def CTF(imSize, DF1, DF2, AST, AmpCon, Cs, kV, apix):
 
     Cs *= 1e7 # Convert Cs to Angstroms
-    DF1 *= 1e4 # Convert defocii from microns to Angstroms 
+    DF1 *= 1e4 # Convert defocii from microns to Angstroms
     DF2 *= 1e4
 
     AST *= -np.pi / 180
@@ -55,15 +55,16 @@ def CTF(imSize, DF1, DF2, AST, AmpCon, Cs, kV, apix):
     xMesh = np.fft.fftfreq(imSize[0])
     yMesh = np.fft.rfftfreq(imSize[1])
 
-    xMeshTile = np.tile(xMesh, [len(yMesh),1]).T
-    yMeshTile = np.tile(yMesh, [len(xMesh),1])
+    xMeshTile = np.tile(xMesh, [len(yMesh), 1]).T
+    yMeshTile = np.tile(yMesh, [len(xMesh), 1])
 
     radialMesh = np.sqrt(xMeshTile * xMeshTile + yMeshTile * yMeshTile) / apix
     angleMesh = np.nan_to_num(np.arctan2(yMeshTile, xMeshTile))
     radialMeshSq = radialMesh * radialMesh
 
     DF = 0.5 * (DF1 + DF2 + (DF1 - DF2) * np.cos(2 * (angleMesh - AST)))
-    Xr = np.nan_to_num(np.pi * WL * radialMeshSq * (DF - 0.5 * WL * WL * radialMeshSq * Cs))
+    Xr = np.nan_to_num(np.pi * WL * radialMeshSq \
+                       * (DF - 0.5 * WL * WL * radialMeshSq * Cs))
 
     CTFim = -w1 * np.sin(Xr) - w2 * np.cos(Xr)
 
