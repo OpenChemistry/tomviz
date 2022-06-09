b292efc0239cedb6f26013bfde0a3541e56c8496
Marcus D. hanwell <mhanwell@bnl.gov>
2022-06-09 14:55:17 -0400

    Reorder the data and then relabel X and Z
    After a fair bit of debugging it is clear that the Python side is silently
    reordering the array to Fortran order before the C++ side sees it. This at
    least gives a correct image stack with the projections on XY and the angles
    along the z axis. Future work to bypass the reorder on the Python side, and
    then drop the C++ side reorder before relabeling.
    
    Signed-off-by: Marcus D. Hanwell <mhanwell@bnl.gov>

--- tomviz/DataBrokerLoadReaction.cxx
+++ tomviz/DataBrokerLoadReaction.cxx
@@ -7,6 +7,7 @@
 #include "DataSource.h"
 #include "LoadDataReaction.h"
 #include "Utilities.h"
+#include "GenericHDF5Format.h"
 
 #include <vtkImageData.h>
 
@@ -42,7 +43,8 @@ void DataBrokerLoadReaction::loadData()
     connect(call, &LoadDataCall::complete, dataBroker,
             [dataBroker, catalog, runUid, table,
              variable](vtkSmartPointer<vtkImageData> imageData) {
-              // relable axes first
+              // Relabel axes first, short-term workaround reorder to C (again).
+              GenericHDF5Format::reorderData(imageData, ReorderMode::FortranToC);
               relabelXAndZAxes(imageData);
               auto dataSource =
                 new DataSource(imageData, DataSource::TiltSeries);
