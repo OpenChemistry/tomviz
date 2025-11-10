47175f4bc19e5b1cbca95d842a0c9c64646b53ab
Patrick Avery <patrick.avery@kitware.com>
2025-11-10 17:28:35 -0600

    Set sys.executable to a valid Python executable
    This is so that libraries like `cpuinfo` can function correctly.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tomviz/python/tomviz/_internal.py
+++ tomviz/python/tomviz/_internal.py
@@ -11,6 +11,7 @@ import fnmatch
 import importlib.machinery
 import importlib.util
 import json
+from pathlib import Path
 import traceback
 
 import tomviz
@@ -19,6 +20,14 @@ import tomviz.operators
 from .fix_pdb import fix_pdb
 
 fix_pdb()
+
+
+# Override sys.executable to contain an actual Python executable, if available
+if not Path(sys.executable).exists():
+    if 'CONDA_PREFIX' in os.environ:
+        try_path = Path(os.environ['CONDA_PREFIX']) / 'bin/python'
+        if try_path.exists():
+            sys.executable = str(try_path)
 
 
 def in_application():
