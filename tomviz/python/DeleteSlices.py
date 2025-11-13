69150d31bb1ca1002a1f424c4dd4b8aac551486c
Patrick Avery <patrick.avery@kitware.com>
2025-11-13 15:03:18 -0600

    Don't allow invalid slices to be deleted
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tomviz/python/DeleteSlices.py
+++ tomviz/python/DeleteSlices.py
@@ -10,6 +10,15 @@ def transform(dataset, firstSlice=None, lastSlice=None, axis=2):
     # Get the current dataset.
     array = dataset.active_scalars
 
+    if lastSlice >= array.shape[axis]:
+        new_value = array.shape[axis] - 1
+        print('WARNING: the lastSlice is past the end of the array. '
+              f'Truncating the lastSlice value to {new_value}')
+        lastSlice = new_value
+
+    if lastSlice < firstSlice:
+        raise Exception(f'Last slice cannot be less than first slice')
+
     # Get indices of the slices to be deleted.
     indices = np.linspace(firstSlice, lastSlice,
                           lastSlice - firstSlice + 1).astype(int)
