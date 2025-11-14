2ac31225ce142115467381ce404d3035453eec65
Patrick Avery <patrick.avery@kitware.com>
2025-11-14 13:50:07 -0600

    Allow all 3x3 transforms with different pixel size
    This even takes into account the padding.
    
    It seems to be working very well for all cases.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tomviz/python/PyStackRegImageAlignment.py
+++ tomviz/python/PyStackRegImageAlignment.py
@@ -19,7 +19,6 @@ def transform(
 ):
     kwargs = {
         'dataset': dataset,
-        'padding': padding,
         'apply_to_all_arrays': apply_to_all_arrays,
         'axis': 2,  # axis hard-coded to two for now
     }
@@ -30,6 +29,7 @@ def transform(
             reference=reference,
             ref_slice_index=ref_slice_index,
             transforms_save_file=transforms_save_file,
+            padding=padding,
             **kwargs,
         )
     elif transform_source == 'from_file':
@@ -77,6 +77,7 @@ def transform_generate(dataset, transform_type='translation', padding=0,
             transform_matrices=tmats,
             spacing=[x for i, x in enumerate(dataset.spacing) if i != axis],
             shape=[x for i, x in enumerate(array.shape) if i != axis],
+            padding=padding,
         )
 
     # Now apply the transform to all specified scalars
@@ -101,9 +102,12 @@ def apply_tmats(sr, dataset, tmats, padding, axis, apply_to_all_arrays):
         dataset.set_scalars(name, output)
 
 
-def transform_from_file(dataset, transform_file, padding=0,
-                        apply_to_all_arrays=True, axis=0):
+def transform_from_file(dataset, transform_file, apply_to_all_arrays=True,
+                        axis=0):
     array = dataset.active_scalars
+
+    # Padding should be zero if we transform from a file
+    padding = 0
 
     # See if we need to rescale the transform matrices
     frame_shape = [x for i, x in enumerate(array.shape) if i != axis]
@@ -114,12 +118,13 @@ def transform_from_file(dataset, transform_file, padding=0,
         transform_type = f['transform_type'].item()
         tmat_spacing = f['spacing']
         tmat_shape = f['shape']
+        tmat_padding = f['padding']
 
     # Rescale the tmats so they can be applied to an image with different
     # physical extents (i. e., tmats generated from XRF can be applied to
     # ptychography, which has a higher resolution in voxel space).
     tmats = rescale_tmats(tmats, transform_type, tmat_shape, tmat_spacing,
-                          frame_shape, spacing)
+                          tmat_padding, frame_shape, spacing)
 
     from pystackreg import StackReg
     sr = StackReg(transform_type_map()[transform_type])
@@ -168,27 +173,55 @@ def register_stack_from_slice(sr, image_stack, ref_idx, axis):
         image_stack[tuple(slices)] = image_stack[tuple(reversed_slices)]
 
 
-def rescale_tmats(tmats, transform_type, shape1, spacing1, shape2,
-                  spacing2):
-    if np.array_equal(shape1, shape2) and np.allclose(spacing1, spacing2):
+def rescale_tmats(tmats, transform_type, shape1, spacing1, padding1,
+                  shape2, spacing2):
+    if np.allclose(spacing1, spacing2) and padding1 == 0:
         # No rescaling needed
         return tmats
 
-    if transform_type != 'translation':
+    if transform_type == 'bilinear':
         msg = (
-            'Shapes or spacings do not match. Currently, transformation '
-            'matrices can only be rescaled if the transformation type is '
-            '`translation`'
+            'Transformation matrix rescaling is not yet supported for '
+            'bilinear transformations.'
         )
         raise Exception(msg)
 
-    w1 = spacing1[0]
-    h1 = spacing1[1]
-    w2 = spacing2[0]
-    h2 = spacing2[1]
+    # Account for changes to padding
+    pad_x_orig = padding1
+    pad_y_orig = padding1
+    pad_x_new = 0
+    pad_y_new = 0
 
-    ret = tmats.copy()
-    # Translation is in the top-right and middle-right numbers.
-    ret[:, 0, 2] *= h1 / h2
-    ret[:, 1, 2] *= w1 / w2
-    return ret
+    # Account for pixel size changes
+    pixel_ratio_x = spacing2[1] / spacing1[1]
+    pixel_ratio_y = spacing2[0] / spacing1[0]
+
+    # We need to account for the padding and pixel resizing changes
+    translate_to_orig = np.array([
+        [1, 0, pad_x_orig - pad_x_new],
+        [0, 1, pad_y_orig - pad_y_new],
+        [0, 0, 1],
+    ])
+    scale_to_orig = np.array([
+        [pixel_ratio_x, 0, 0],
+        [0, pixel_ratio_y, 0],
+        [0, 0, 1],
+    ])
+    scale_to_new = np.array([
+        [1 / pixel_ratio_x, 0, 0],
+        [0, 1 / pixel_ratio_y, 0],
+        [0, 0, 1],
+    ])
+    translate_to_new = np.array([
+        [1, 0, -(pad_x_orig - pad_x_new)],
+        [0, 1, -(pad_y_orig - pad_y_new)],
+        [0, 0, 1],
+    ])
+
+    return (
+        translate_to_new @
+        scale_to_new @
+        tmats @
+        scale_to_orig @
+        translate_to_orig
+    )
