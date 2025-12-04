4ae088542fc930dab3504f68af6d2235fcaf2f9a
Patrick Avery <patrick.avery@kitware.com>
2025-12-04 12:33:24 -0600

    Normalize tilt series for each array
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- tomviz/python/NormalizeTiltSeries.py
+++ tomviz/python/NormalizeTiltSeries.py
@@ -1,3 +1,7 @@
+from tomviz.utils import apply_to_each_array
+
+
+@apply_to_each_array
 def transform(dataset):
     """
     Normalize tilt series so that each tilt image has the same total intensity.
